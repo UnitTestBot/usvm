2e9bbf6f7ceba3bed113bf73e7c2ef9cbd0d51fe
Alexey Menshutin <alex.menshutin99@gmail.com>
2023-04-28 12:33:51 +0300

    Sort visitor

--- usvm-core/src/main/kotlin/org/usvm/solver/USoftConstraintsProvider.kt
+++ usvm-core/src/main/kotlin/org/usvm/solver/USoftConstraintsProvider.kt
@@ -8,7 +8,11 @@ import org.ksmt.sort.KArray3Sort
 import org.ksmt.sort.KArrayNSort
 import org.ksmt.sort.KArraySort
 import org.ksmt.sort.KBoolSort
+import org.ksmt.sort.KBv1Sort
+import org.ksmt.sort.KBv8Sort
 import org.ksmt.sort.KBvSort
+import org.ksmt.sort.KFp32Sort
+import org.ksmt.sort.KFp64Sort
 import org.ksmt.sort.KFpRoundingModeSort
 import org.ksmt.sort.KFpSort
 import org.ksmt.sort.KIntSort
@@ -16,6 +20,7 @@ import org.ksmt.sort.KRealSort
 import org.ksmt.sort.KSort
 import org.ksmt.sort.KSortVisitor
 import org.ksmt.sort.KUninterpretedSort
+import org.ksmt.utils.cast
 import org.usvm.UAddressSort
 import org.usvm.UAllocatedArrayReading
 import org.usvm.UBoolExpr
@@ -35,6 +40,7 @@ import org.usvm.URegisterReading
 import org.usvm.USizeExpr
 import org.usvm.USort
 import org.usvm.USymbol
+import org.usvm.uctx
 
 class USoftConstraintsProvider<Field, Type>(ctx: UContext) : UExprTransformer<Field, Type>(ctx) {
     private val caches: MutableMap<UExpr<*>, UBoolExpr> = hashMapOf()
@@ -57,7 +63,7 @@ class USoftConstraintsProvider<Field, Type>(ctx: UContext) : UExprTransformer<Fi
 
 
     override fun <T : KSort> transformExpr(expr: KExpr<T>): KExpr<T> = computeSideEffect(expr) {
-        caches[expr] = expr.sort.accept(sortPreferredValues)
+        caches[expr] = expr.sort.accept(sortPreferredValues)(expr)
     }
 
     override fun <T : KSort, A : KSort> transformApp(expr: KApp<T, A>): KExpr<T> =
@@ -66,7 +72,7 @@ class USoftConstraintsProvider<Field, Type>(ctx: UContext) : UExprTransformer<Fi
                 val collected = args.fold(expr.ctx.trueExpr as UBoolExpr) { acc, value ->
                     expr.ctx.mkAnd(acc, caches.getValue(value), flat = false)
                 }
-                val selfConstraint = expr.sort.accept(sortPreferredValues)
+                val selfConstraint = expr.sort.accept(sortPreferredValues)(expr)
                 caches[expr] = expr.ctx.mkAnd(selfConstraint, collected, flat = false)
             }
         }
@@ -88,7 +94,7 @@ class USoftConstraintsProvider<Field, Type>(ctx: UContext) : UExprTransformer<Fi
     }
 
     override fun <Sort : USort> transform(expr: URegisterReading<Sort>): UExpr<Sort> = with(expr.ctx) {
-        caches[expr] = expr.sort.accept(sortPreferredValues)
+        caches[expr] = expr.sort.accept(sortPreferredValues)(expr)
         expr
     }
 
@@ -139,48 +145,83 @@ class USoftConstraintsProvider<Field, Type>(ctx: UContext) : UExprTransformer<Fi
     // endregion
 }
 
-private class SortPreferredValues() : KSortVisitor<KExpr<KBoolSort>> {
-    override fun <S : KBvSort> visit(sort: S): KExpr<KBoolSort> = with(sort.ctx) {
-        TODO()
+// TODO choose between KSort and USort
+private class SortPreferredValues : KSortVisitor<(KExpr<*>) -> KExpr<KBoolSort>> {
+    val caches: MutableMap<USort, (KExpr<*>) -> KExpr<KBoolSort>> = mutableMapOf()
+
+    override fun <S : KBvSort> visit(sort: S): (KExpr<*>) -> KExpr<KBoolSort> = caches.getOrPut(sort) {
+        with(sort.ctx) {
+            when (sort) {
+                is KBv1Sort -> { expr -> 1.toBv(sort) eq expr.cast() }
+                is KBv8Sort -> { expr -> createBvBounds(lowerBound = -32, upperBound = 32, expr) }
+                else -> { expr ->
+                    if (sort.sizeBits < 16u) {
+                        createBvBounds(lowerBound = -8, upperBound = 8, expr)
+                    } else {
+                        createBvBounds(lowerBound = -256, upperBound = 256, expr)
+                    }
+                }
+            }
+        }
     }
 
-    override fun <S : KFpSort> visit(sort: S): KExpr<KBoolSort> {
-        TODO()
+    private fun createBvBounds(
+        lowerBound: Int,
+        upperBound: Int,
+        expr: UExpr<*>,
+    ): UBoolExpr = with(expr.ctx) {
+        mkAnd(
+            mkBvSignedLessExpr(lowerBound.toBv(expr.sort.cast()), expr.cast()),
+            mkBvSignedGreaterExpr(upperBound.toBv(expr.sort.cast()), expr.cast())
+        )
     }
 
-    override fun <D0 : KSort, D1 : KSort, R : KSort> visit(sort: KArray2Sort<D0, D1, R>): KExpr<KBoolSort> {
-        return sort.range.accept(this)
+    override fun <S : KFpSort> visit(sort: S): (KExpr<*>) -> KExpr<KBoolSort> = caches.getOrPut(sort) {
+        with(sort.ctx) {
+            when (sort) {
+                is KFp32Sort -> { expr -> createFpBounds(expr) }
+                is KFp64Sort -> { expr -> createFpBounds(expr) }
+                else -> { expr -> createFpBounds(expr) }
+            }
+        }
     }
 
-    override fun <D0 : KSort, D1 : KSort, D2 : KSort, R : KSort> visit(sort: KArray3Sort<D0, D1, D2, R>): KExpr<KBoolSort> {
-        return sort.range.accept(this)
-    }
+    private fun createFpBounds(expr: UExpr<*>): UBoolExpr = expr.ctx.trueExpr // TODO
 
-    override fun <R : KSort> visit(sort: KArrayNSort<R>): KExpr<KBoolSort> {
-        return sort.range.accept(this)
-    }
+    override fun <D0 : KSort, D1 : KSort, R : KSort> visit(
+        sort: KArray2Sort<D0, D1, R>,
+    ): (KExpr<*>) -> KExpr<KBoolSort> = sort.range.accept(this)
 
-    override fun <D : KSort, R : KSort> visit(sort: KArraySort<D, R>): KExpr<KBoolSort> {
-        TODO("Not yet implemented")
-    }
+    override fun <D0 : KSort, D1 : KSort, D2 : KSort, R : KSort> visit(
+        sort: KArray3Sort<D0, D1, D2, R>,
+    ): (KExpr<*>) -> KExpr<KBoolSort> = sort.range.accept(this)
 
-    override fun visit(sort: KBoolSort): KExpr<KBoolSort> {
-        TODO("Not yet implemented")
-    }
+    override fun <R : KSort> visit(
+        sort: KArrayNSort<R>,
+    ): (KExpr<*>) -> KExpr<KBoolSort> = sort.range.accept(this)
 
-    override fun visit(sort: KFpRoundingModeSort): KExpr<KBoolSort> {
-        TODO("Not yet implemented")
-    }
+    override fun <D : KSort, R : KSort> visit(
+        sort: KArraySort<D, R>,
+    ): (KExpr<*>) -> KExpr<KBoolSort> = sort.range.accept(this)
 
-    override fun visit(sort: KIntSort): KExpr<KBoolSort> {
-        TODO("Not yet implemented")
-    }
+    override fun visit(sort: KBoolSort): (KExpr<*>) -> KExpr<KBoolSort> = { sort.ctx.trueExpr } // TODO ???
 
-    override fun visit(sort: KRealSort): KExpr<KBoolSort> {
-        TODO("Not yet implemented")
-    }
+    override fun visit(sort: KFpRoundingModeSort): (KExpr<*>) -> KExpr<KBoolSort> = { sort.ctx.trueExpr }
 
-    override fun visit(sort: KUninterpretedSort): KExpr<KBoolSort> {
-        TODO("Not yet implemented")
-    }
+    override fun visit(sort: KIntSort): (KExpr<*>) -> KExpr<KBoolSort> = { sort.ctx.trueExpr } // TODO replace
+
+    override fun visit(sort: KRealSort): (KExpr<*>) -> KExpr<KBoolSort> = { sort.ctx.trueExpr } // TODO replace
+
+    override fun visit(sort: KUninterpretedSort): (KExpr<*>) -> KExpr<KBoolSort> =
+        caches.getOrPut(sort) {
+            with(sort.uctx) {
+                when {
+                    sort === addressSort -> { expr ->
+                        mkHeapRefEq(nullRef, expr.cast())
+                    }
+
+                    else -> { _ -> trueExpr } // TODO ?????
+                }
+            }
+        }
 }
