b83585289f8af567a344f4685718754b31fdc356
DaniilStepanov <stepanov0995@mail.ru>
2023-05-30 15:29:07 +0300

    UTest executor

---
+++ usvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/InstrumentationProcessRunner.kt
@@ -0,0 +1,69 @@
+package org.usvm.instrumentation
+
+import com.jetbrains.rd.framework.util.NetUtils
+import com.jetbrains.rd.util.lifetime.Lifetime
+import com.jetbrains.rd.util.lifetime.LifetimeDefinition
+import com.jetbrains.rd.util.lifetime.isAlive
+import org.jacodb.api.JcClasspath
+import org.usvm.instrumentation.executor.ProcessRunner
+import org.usvm.instrumentation.jacodb.transform.JcInstrumenter
+import org.usvm.instrumentation.jacodb.transform.JcInstrumenterFactory
+import org.usvm.instrumentation.rd.InstrumentedProcess
+import org.usvm.instrumentation.testcase.UTest
+import org.usvm.instrumentation.testcase.statement.UTestExecutionResult
+import org.usvm.instrumentation.util.InstrumentationModuleConstants
+import osSpecificJavaExecutable
+import java.io.File
+import kotlin.reflect.KClass
+
+class InstrumentationProcessRunner(
+    private val testingProjectClasspath: String,
+    private val jcClasspath: JcClasspath,
+    private val instrumentationClassFactory: KClass<out JcInstrumenterFactory<out JcInstrumenter>>
+) {
+
+    private lateinit var processRunner: ProcessRunner
+    private lateinit var lifetime: Lifetime
+
+    constructor(
+        testingProjectClasspath: List<String>,
+        jcClasspath: JcClasspath,
+        instrumentationClassFactory: KClass<JcInstrumenterFactory<out JcInstrumenter>>
+    ) : this(testingProjectClasspath.joinToString(":"), jcClasspath, instrumentationClassFactory)
+
+    fun isAlive() = this::lifetime.isInitialized && lifetime.isAlive
+
+    private val workerProcessArgs: List<String> by lazy {
+        val instrumentationClassNameFactoryName = instrumentationClassFactory.java.name
+        val memoryLimit = listOf("-Xmx1g")
+        val pathToJava = JdkInfoService.provide().path
+        val usvmClasspath = System.getProperty("java.class.path")
+        val javaVersionSpecificArguments = OpenModulesContainer.javaVersionSpecificArguments
+        val instrumentedProcessClassName =
+            InstrumentedProcess::class.qualifiedName ?: error("Can't find instumented process")
+        listOf(pathToJava.resolve("bin${File.separatorChar}${osSpecificJavaExecutable()}").toString()) +
+                listOf("-javaagent:${InstrumentationModuleConstants.pathToUsvmInstrumentationJar}=$instrumentationClassNameFactoryName") +
+                memoryLimit +
+                javaVersionSpecificArguments +
+                listOf("-classpath", usvmClasspath) +
+                listOf(instrumentedProcessClassName, testingProjectClasspath)
+    }
+
+    suspend fun init(parentLifetime: Lifetime) {
+        val processLifetime = LifetimeDefinition(parentLifetime)
+        lifetime = processLifetime
+        val rdPort = NetUtils.findFreePort(0)
+        val workerCommand = workerProcessArgs + listOf("$rdPort")
+        val pb = ProcessBuilder(workerCommand).inheritIO()
+        val process = pb.start()
+        processRunner =
+            ProcessRunner(process = process, rdPort = rdPort, jcClasspath = jcClasspath, lifetime = processLifetime)
+        processRunner.init()
+    }
+
+    suspend fun executeUTest(uTest: UTest): UTestExecutionResult = processRunner.callUTest(uTest)
+
+    suspend fun destroy() = processRunner.destroy()
+
+
+}
--- usvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/agent/ClassTransformer.kt
+++ usvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/agent/ClassTransformer.kt
@@ -1,17 +1,18 @@
 package org.usvm.instrumentation.agent
 
-import org.jacodb.api.ext.findClass
-import org.jacodb.api.ext.methods
-import org.jacodb.impl.fs.ClassSourceImpl
-import org.objectweb.asm.tree.ClassNode
 import org.usvm.instrumentation.classloader.WorkerClassLoader
-import org.usvm.instrumentation.jacodb.transform.RuntimeTraceInstrumenter
+import org.usvm.instrumentation.jacodb.transform.JcInstrumenter
+import org.usvm.instrumentation.jacodb.transform.JcInstrumenterFactory
+import org.usvm.instrumentation.jacodb.transform.JcRuntimeTraceInstrumenter
 import org.usvm.instrumentation.trace.collector.TraceCollector
 import org.usvm.instrumentation.util.*
 import java.lang.instrument.ClassFileTransformer
 import java.security.ProtectionDomain
 
-class ClassTransformer : ClassFileTransformer {
+class ClassTransformer(instrumenterClassName: String) : ClassFileTransformer {
+
+    private val instrumenterFactoryInstance =
+        Class.forName(instrumenterClassName).constructors.first().newInstance() as JcInstrumenterFactory<*>
 
     override fun transform(
         loader: ClassLoader?,
@@ -23,7 +24,7 @@ class ClassTransformer : ClassFileTransformer {
         if (loader !is WorkerClassLoader) {
             return super.transform(loader, className, classBeingRedefined, protectionDomain, classfileBuffer)
         }
-        val instrumenter = RuntimeTraceInstrumenter(loader.jcClasspath, TraceCollector())
+        val instrumenter = instrumenterFactoryInstance.create(loader.jcClasspath)
         return instrumenter.instrumentClass(classfileBuffer.toClassNode()).toByteArray(loader, checkClass = true)
     }
 
