0936d55dc1f5b5f873c625ecf142853da1c54594
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-06-07 13:13:47 +0300

    Updated API; further experiments

--- usvm-python/src/main/java/org/usvm/interpreter/CPythonAdapter.java
+++ usvm-python/src/main/java/org/usvm/interpreter/CPythonAdapter.java
@@ -1,8 +1,8 @@
 package org.usvm.interpreter;
 
 import io.ksmt.expr.KExpr;
-import org.usvm.language.Instruction;
-import org.usvm.language.Symbol;
+import org.usvm.language.PythonOPCODE;
+import org.usvm.language.SymbolForCPython;
 
 import java.util.function.Supplier;
 
@@ -17,7 +17,7 @@ public class CPythonAdapter {
     public native long getNewNamespace();  // returns reference to a new dict
     public native int concreteRun(long globals, String code);  // returns 0 on success
     public native long eval(long globals, String expr);  // returns PyObject *
-    public native int concolicRun(long globals, long functionRef, long[] concreteArgs, Symbol[] symbolicArgs, ConcolicRunContext context);
+    public native int concolicRun(long globals, long functionRef, long[] concreteArgs, SymbolForCPython[] symbolicArgs, ConcolicRunContext context);
 
     static {
         System.loadLibrary("cpythonadapter");
@@ -27,70 +27,70 @@ public class CPythonAdapter {
         context.instructionCounter++;
         // TODO: check consistency
         if (context.instructionCounter > context.curState.getPath().size())
-            context.curState.setPath(context.curState.getPath().add(new Instruction(instruction)));
+            context.curState.setPath(context.curState.getPath().add(new PythonOPCODE(instruction)));
     }
 
     @SuppressWarnings("rawtypes")
-    private static <T extends KExpr> Symbol methodWrapper(ConcolicRunContext context, Supplier<T> valueSupplier) {
+    private static <T extends KExpr> SymbolForCPython methodWrapper(ConcolicRunContext context, Supplier<T> valueSupplier) {
         T result = valueSupplier.get();
         if (result != null)
-            return new Symbol(result);
+            return new SymbolForCPython(result);
         return null;
     }
 
-    public static Symbol handlerLoadConstLong(ConcolicRunContext context, long value) {
-        return new Symbol(context.ctx.mkIntNum(value));
+    public static SymbolForCPython handlerLoadConstLong(ConcolicRunContext context, long value) {
+        return methodWrapper(context, () -> context.ctx.mkIntNum(value));
     }
 
-    public static void handlerFork(ConcolicRunContext context, Symbol cond) {
+    public static void handlerFork(ConcolicRunContext context, SymbolForCPython cond) {
         handlerForkKt(context, cond.expr);
     }
 
-    public static Symbol handlerGTLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerGTLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerGTLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerLTLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerLTLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerLTLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerEQLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerEQLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerEQLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerNELong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerNELong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerNELongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerGELong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerGELong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerGELongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerLELong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerLELong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerLELongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerADDLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerADDLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerADDLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerSUBLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerSUBLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerSUBLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerMULLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerMULLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerMULLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerDIVLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerDIVLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerDIVLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerREMLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerREMLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerREMLongKt(context.ctx, left.expr, right.expr));
     }
 
-    public static Symbol handlerPOWLong(ConcolicRunContext context, Symbol left, Symbol right) {
+    public static SymbolForCPython handlerPOWLong(ConcolicRunContext context, SymbolForCPython left, SymbolForCPython right) {
         return methodWrapper(context, () -> handlerPOWLongKt(context.ctx, left.expr, right.expr));
     }
 }
--- usvm-python/src/main/java/org/usvm/language/SymbolForCPython.java
+++ usvm-python/src/main/java/org/usvm/language/SymbolForCPython.java
@@ -3,9 +3,9 @@ package org.usvm.language;
 import io.ksmt.expr.KExpr;
 
 @SuppressWarnings("rawtypes")
-public class Symbol {
+public class SymbolForCPython {
     public KExpr expr;
-    public Symbol(KExpr expr) {
+    public SymbolForCPython(KExpr expr) {
         this.expr = expr;
     }
 }
