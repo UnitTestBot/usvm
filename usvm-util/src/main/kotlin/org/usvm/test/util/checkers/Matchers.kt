4e26240258e9e2ad2b2876f291aa4c1475358b32
Alexey Menshutin <alex.menshutin99@gmail.com>
2023-06-09 13:46:39 +0300

    Add base class for interpreters testing (#23)

---
+++ usvm-util/src/main/kotlin/org/usvm/test/util/checkers/Matchers.kt
@@ -0,0 +1,25 @@
+package org.usvm.test.util.checkers
+
+fun ge(count: Int) = AnalysisResultsNumberMatcher(
+    description = "ge $count",
+    matcherFailedMessage = { "Expected at least $count executions, but only $it found" }
+) { it >= count }
+
+fun eq(count: Int) = AnalysisResultsNumberMatcher(
+    description = "eq $count",
+    matcherFailedMessage = { "Expected exactly $count executions, but $it found" }
+) { it == count }
+
+fun between(bounds: IntRange) = AnalysisResultsNumberMatcher(
+    description = "$bounds",
+    matcherFailedMessage = { "Expected number of executions in bounds $bounds, but $it found" }
+) { it in bounds }
+
+class AnalysisResultsNumberMatcher(
+    private val description: String,
+    val matcherFailedMessage: (Int) -> String,
+    private val cmp: (Int) -> Boolean,
+) {
+    operator fun invoke(x: Int) = cmp(x)
+    override fun toString() = description
+}
