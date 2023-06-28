9a452bd61e2018c9b28edefec61bdc760c62f859
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-06-28 15:14:09 +0300

    Added path diversion detection

--- usvm-python/src/main/kotlin/org/usvm/language/Instruction.kt
+++ usvm-python/src/main/kotlin/org/usvm/language/Instruction.kt
@@ -1,9 +1,3 @@
 package org.usvm.language
 
-sealed class PythonInstruction
-data class PythonOPCODE(val numberInBytecode: Int): PythonInstruction()
-data class MethodQuery(
-    val methodId: Int,
-    val operands: List<SymbolForCPython>,
-    val result: SymbolForCPython
-): PythonInstruction()
+data class PythonInstruction(val numberInBytecode: Int)
