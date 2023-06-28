d06d0b230955cb5221742e4a3cd9bcaf48b261df
DaniilStepanov <stepanov0995@mail.ru>
2023-06-28 18:07:46 +0300

    Mock implementation almost work

--- usvm-instrumentation/src/samples/java/example/MockAbstractClass.java
+++ usvm-instrumentation/src/samples/java/example/MockAbstractClass.java
@@ -3,11 +3,16 @@ package example;
 public abstract class MockAbstractClass {
 
     public String stringField = "";
-    public int intField;
+    public int intField = 238;
+
 
     public abstract int getI();
 
     public String getStr() { return "zzz"; }
+
+    public int methodWithInternalInvocation() {
+        return intField + getI();
+    }
 
 
 }
