8591ac85bbf03a6433a77d8884de5e06665e665d
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-06-29 16:18:31 +0300

    Fixed PythonTestRunner

--- usvm-python/src/test/kotlin/org/usvm/samples/PythonTestRunner.kt
+++ usvm-python/src/test/kotlin/org/usvm/samples/PythonTestRunner.kt
@@ -1,70 +1,54 @@
 package org.usvm.samples
 
-import org.usvm.interpreter.PythonExecutionState
+import org.usvm.interpreter.ConcretePythonInterpreter
+import org.usvm.interpreter.PythonAnalysisResult
 import org.usvm.interpreter.PythonMachine
 import org.usvm.language.PythonCallable
+import org.usvm.language.PythonInt
 import org.usvm.language.PythonProgram
+import org.usvm.language.PythonType
 import org.usvm.test.util.TestRunner
+import org.usvm.test.util.checkers.AnalysisResultsNumberMatcher
 import java.io.File
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTimedValue
 
-fun main() {
-    val functionDescriptor = FunctionDescriptor(
-        fileName = "./src/samples/python/SimpleExample.py",
-        functionName = "f",
-        numberOfArguments = 3
-    )
-    PythonTestRunner().internalCheck(functionDescriptor)
-}
-
-class PythonTestRunner : TestRunner<PythonTest, FunctionDescriptor, Any, PythonCoverage>() {
-
-    @OptIn(ExperimentalTime::class)
-    fun internalCheck(
-        functionDescriptor: FunctionDescriptor,
-    ) {
-        val file = File(functionDescriptor.fileName)
-        val program = PythonProgram(file.readText())
-        val function = PythonCallable.constructCallableFromName(
-            functionDescriptor.numberOfArguments,
-            functionDescriptor.functionName
-        )
-
-        val machine = PythonMachine(program)
-
-        val (results, time) = measureTimedValue {
-            machine.use {
-                it.analyzeWithProducingStates(function)
-            }
+open class PythonTestRunner(sourcePath: String) : TestRunner<PythonTest, PythonCallable, PythonType, PythonCoverage>() {
+    private val testSources = File(PythonTestRunner::class.java.getResource(sourcePath)!!.file).readText()
+    private val machine = PythonMachine(PythonProgram(testSources)) { pythonObject ->
+        ConcretePythonInterpreter.getPythonObjectRepr(pythonObject)
+    }
+    override val typeTransformer: (Any?) -> PythonType
+        get() = { _ -> PythonInt }
+    override val checkType: (PythonType, PythonType) -> Boolean
+        get() = { _, _ -> true }
+    override val runner: (PythonCallable) -> List<PythonTest>
+        get() = { callable ->
+            val results: MutableList<PythonAnalysisResult<String>> = mutableListOf()
+            machine.analyze(callable, results)
+            results
         }
+    override val coverageRunner: (List<PythonTest>) -> PythonCoverage
+        get() = { _ -> PythonCoverage(0) }
 
-        val tests = results.extractInputValuesWithResult()
-
-        println("Finished in $time milliseconds.")
-        println(tests)
+    private inline fun <reified FUNCTION_TYPE : Function<Boolean>> createCheckReprs():
+                (PythonCallable, AnalysisResultsNumberMatcher, List<FUNCTION_TYPE>, List<FUNCTION_TYPE>) -> Unit = {
+        target: PythonCallable,
+        analysisResultsNumberMatcher: AnalysisResultsNumberMatcher,
+        invariants: List<FUNCTION_TYPE>,
+        propertiesToDiscover: List<FUNCTION_TYPE> ->
+        internalCheck(
+            target,
+            analysisResultsNumberMatcher,
+            propertiesToDiscover.toTypedArray(),
+            invariants.toTypedArray(),
+            { pythonTest -> pythonTest.inputValues.map { it.reprFromPythonObject } + pythonTest.result },
+            (target.signature.map { PythonInt } + PythonInt).toTypedArray(),
+            CheckMode.MATCH_PROPERTIES,
+            { true }
+        )
     }
 
-    private fun List<PythonExecutionState>.extractInputValuesWithResult(): List<PythonTest> =
-        this.map {
-            TODO()
-        }
-
-    override val typeTransformer: (Any?) -> Any
-        get() = { _ -> TODO() }
-    override val checkType: (Any, Any) -> Boolean
-        get() = { _, _ -> true }
-    override val runner: (FunctionDescriptor) -> List<PythonTest>
-        get() = TODO("Not yet implemented")
-    override val coverageRunner: (List<PythonTest>) -> PythonCoverage
-        get() = TODO("Not yet implemented")
+    protected val checkReprs3 = createCheckReprs<(String, String, String, String) -> Boolean>()
 }
 
-data class FunctionDescriptor(
-    val fileName: String,
-    val functionName: String,
-    val numberOfArguments: Int
-)
-
-data class PythonTest(val inputValues: List<Any?>, val result: Any?)
+typealias PythonTest = PythonAnalysisResult<String>
 data class PythonCoverage(val int: Int)
