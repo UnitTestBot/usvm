7594c2b44b63236be078624133d36edc87bbb76f
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-06-29 17:45:45 +0300

    Add concolic and concrete run comparison in PythonTestRunner

--- usvm-python/src/test/kotlin/org/usvm/samples/SimpleExampleTest.kt
+++ usvm-python/src/test/kotlin/org/usvm/samples/SimpleExampleTest.kt
@@ -1,6 +1,7 @@
 package org.usvm.samples
 
 import org.junit.jupiter.api.Test
+import org.usvm.interpreter.ConcretePythonInterpreter
 import org.usvm.language.PythonCallable
 import org.usvm.language.PythonInt
 import org.usvm.test.util.checkers.ignoreNumberOfAnalysisResults
@@ -9,13 +10,15 @@ class SimpleExampleTest : PythonTestRunner("/samples/SimpleExample.py") {
     private val functionF = PythonCallable.constructCallableFromName(List(3) { PythonInt }, "f")
     @Test
     fun testF() {
-        checkReprs3(
+        checkReprs3WithConcreteRun(
             functionF,
             ignoreNumberOfAnalysisResults,
             /* invariants = */ emptyList(),
             /* propertiesToDiscover = */ List(10) { index ->
                 { _, _, _, res -> res == index.toString() }
             }
-        )
+        ) /* compareConcolicAndConcrete */ { testFromConcolic, concreteResult ->
+            testFromConcolic.result == ConcretePythonInterpreter.getPythonObjectRepr(concreteResult)
+        }
     }
 }
