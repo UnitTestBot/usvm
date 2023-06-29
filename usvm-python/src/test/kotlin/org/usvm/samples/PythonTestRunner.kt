d76e0f51f059d141e13487bb2a8d2e59f6617980
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-06-29 17:45:45 +0300

    Add concolic and concrete run comparison in PythonTestRunner

--- usvm-python/src/test/kotlin/org/usvm/samples/PythonTestRunner.kt
+++ usvm-python/src/test/kotlin/org/usvm/samples/PythonTestRunner.kt
@@ -1,8 +1,6 @@
 package org.usvm.samples
 
-import org.usvm.interpreter.ConcretePythonInterpreter
-import org.usvm.interpreter.PythonAnalysisResult
-import org.usvm.interpreter.PythonMachine
+import org.usvm.interpreter.*
 import org.usvm.language.PythonCallable
 import org.usvm.language.PythonInt
 import org.usvm.language.PythonProgram
@@ -29,25 +27,59 @@ open class PythonTestRunner(sourcePath: String) : TestRunner<PythonTest, PythonC
     override val coverageRunner: (List<PythonTest>) -> PythonCoverage
         get() = { _ -> PythonCoverage(0) }
 
+    private fun compareWithConcreteRun(
+        target: PythonCallable,
+        test: PythonTest,
+        check: (PythonObject) -> Boolean
+    ): Boolean {
+        val namespace = ConcretePythonInterpreter.getNewNamespace()
+        ConcretePythonInterpreter.concreteRun(namespace, testSources)
+        val functionRef = target.reference(namespace)
+        val converter = ConverterToPythonObject(namespace)
+        val args = test.inputValues.map { converter.convert(it.asUExpr, it.type)!! }
+        val concreteResult = ConcretePythonInterpreter.concreteRunOnFunctionRef(namespace, functionRef, args)
+        return check(concreteResult)
+    }
+
+    private inline fun <reified FUNCTION_TYPE : Function<Boolean>> createCheckReprsWithConcreteRun(concreteRun: Boolean = true):
+                (PythonCallable, AnalysisResultsNumberMatcher, List<FUNCTION_TYPE>, List<FUNCTION_TYPE>, (PythonTest, PythonObject) -> Boolean) -> Unit = {
+            target: PythonCallable,
+            analysisResultsNumberMatcher: AnalysisResultsNumberMatcher,
+            invariants: List<FUNCTION_TYPE>,
+            propertiesToDiscover: List<FUNCTION_TYPE>,
+            compareConcolicAndConcrete: (PythonTest, PythonObject) -> Boolean ->
+        val onAnalysisResult = { pythonTest: PythonTest ->
+            val result = pythonTest.inputValues.map { it.reprFromPythonObject } + pythonTest.result
+            if (concreteRun) {
+                require(compareWithConcreteRun(target, pythonTest) { compareConcolicAndConcrete(pythonTest, it) }) {
+                    "Error in CPython patch: concrete and concolic results differ"
+                }
+            }
+            result
+        }
+        internalCheck(
+            target,
+            analysisResultsNumberMatcher,
+            propertiesToDiscover.toTypedArray(),
+            invariants.toTypedArray(),
+            onAnalysisResult,
+            (target.signature.map { PythonInt } + PythonInt).toTypedArray(),
+            CheckMode.MATCH_PROPERTIES,
+            coverageChecker = { true }
+        )
+    }
+
     private inline fun <reified FUNCTION_TYPE : Function<Boolean>> createCheckReprs():
                 (PythonCallable, AnalysisResultsNumberMatcher, List<FUNCTION_TYPE>, List<FUNCTION_TYPE>) -> Unit = {
         target: PythonCallable,
         analysisResultsNumberMatcher: AnalysisResultsNumberMatcher,
         invariants: List<FUNCTION_TYPE>,
         propertiesToDiscover: List<FUNCTION_TYPE> ->
-        internalCheck(
-            target,
-            analysisResultsNumberMatcher,
-            propertiesToDiscover.toTypedArray(),
-            invariants.toTypedArray(),
-            { pythonTest -> pythonTest.inputValues.map { it.reprFromPythonObject } + pythonTest.result },
-            (target.signature.map { PythonInt } + PythonInt).toTypedArray(),
-            CheckMode.MATCH_PROPERTIES,
-            { true }
-        )
+        createCheckReprsWithConcreteRun<FUNCTION_TYPE>(concreteRun = false)(target, analysisResultsNumberMatcher, invariants, propertiesToDiscover) { _, _ -> true }
     }
 
     protected val checkReprs3 = createCheckReprs<(String, String, String, String) -> Boolean>()
+    protected val checkReprs3WithConcreteRun = createCheckReprsWithConcreteRun<(String, String, String, String) -> Boolean>()
 }
 
 typealias PythonTest = PythonAnalysisResult<String>
