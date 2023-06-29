1cf268800423103997b1341c598f538b26caca3e
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-06-29 11:19:08 +0300

    Added customization of PythonObject serialization

--- usvm-python/src/main/kotlin/org/usvm/interpreter/USVMPythonInterpreter.kt
+++ usvm-python/src/main/kotlin/org/usvm/interpreter/USVMPythonInterpreter.kt
@@ -4,11 +4,13 @@ import org.usvm.*
 import org.usvm.language.PythonCallable
 import org.usvm.language.PythonProgram
 
-class USVMPythonInterpreter(
+class USVMPythonInterpreter<PYTHON_OBJECT_REPRESENTATION>(
     private val ctx: UContext,
     private val program: PythonProgram,
     private val callable: PythonCallable,
-    private val iterationCounter: IterationCounter
+    private val iterationCounter: IterationCounter,
+    private val pythonObjectSerialization: (PythonObject) -> PYTHON_OBJECT_REPRESENTATION,
+    private val saveRunResult: (List<PYTHON_OBJECT_REPRESENTATION>, PYTHON_OBJECT_REPRESENTATION?) -> Unit
 ) : UInterpreter<PythonExecutionState>() {
     private fun prepareNamespace(): PythonNamespace {
         val namespace = ConcretePythonInterpreter.getNewNamespace()
@@ -18,27 +20,20 @@ class USVMPythonInterpreter(
 
     override fun step(state: PythonExecutionState): StepResult<PythonExecutionState> =
         with(ctx) {
-            //println("Step on $state. ${state.wasExecuted}. Executed path: ${state.path}")
-            //println(state.pathConstraints.logicalConstraints)
-            //System.out.flush()
             val symbols = state.inputSymbols
             val seeds = symbols.map { state.models.first().eval(it.expr) }
             val namespace = prepareNamespace()
             val converter = ConverterToPythonObject(namespace)
             val functionRef = callable.reference(namespace)
             val concrete = (seeds zip callable.signature).map { (seed, type) ->
-                //println("Concrete: $seed")
-                //System.out.flush()
                 converter.convert(seed, type) ?: error("Couldn't construct PythonObject from model")
             }
+            val serializedInputs = concrete.map(pythonObjectSerialization)
             val concolicRunContext = ConcolicRunContext(state, ctx)
             val result = ConcretePythonInterpreter.concolicRun(namespace, functionRef, concrete, symbols, concolicRunContext)
+            val serializedResult = pythonObjectSerialization(result)
+            saveRunResult(serializedInputs, serializedResult)
             concolicRunContext.curState.wasExecuted = true
-            ConcretePythonInterpreter.printPythonObject(result)
-            //println("Finished with state: ${concolicRunContext.curState}. ${concolicRunContext.curState.pathConstraints.logicalConstraints}")
-            //println("Forked states: ${concolicRunContext.forkedStates}")
-            //println("Result of step: ${result.forkedStates.take(10).toList()}")
-            //println("Number of forks: ${concolicRunContext.forkedStates.size}")
             iterationCounter.iterations += 1
             return StepResult(concolicRunContext.forkedStates.asSequence(), !state.wasExecuted)
         }
