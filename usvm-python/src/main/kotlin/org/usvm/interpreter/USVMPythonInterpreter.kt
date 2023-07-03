a0bfb4ee1de5c2fccee209008594d92d1fe62b5e
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-07-03 15:28:52 +0300

    Added processing of python function calls

--- usvm-python/src/main/kotlin/org/usvm/interpreter/USVMPythonInterpreter.kt
+++ usvm-python/src/main/kotlin/org/usvm/interpreter/USVMPythonInterpreter.kt
@@ -1,37 +1,30 @@
 package org.usvm.interpreter
 
 import org.usvm.*
-import org.usvm.language.PythonCallable
-import org.usvm.language.PythonProgram
 import org.usvm.language.PythonType
+import org.usvm.language.PythonUnpinnedCallable
 
 class USVMPythonInterpreter<PYTHON_OBJECT_REPRESENTATION>(
     private val ctx: UContext,
-    private val program: PythonProgram,
-    private val callable: PythonCallable,
+    private val namespace: PythonNamespace,
+    private val callable: PythonUnpinnedCallable,
     private val iterationCounter: IterationCounter,
     private val pythonObjectSerialization: (PythonObject) -> PYTHON_OBJECT_REPRESENTATION,
     private val saveRunResult: (PythonAnalysisResult<PYTHON_OBJECT_REPRESENTATION>) -> Unit
 ) : UInterpreter<PythonExecutionState>() {
-    private fun prepareNamespace(): PythonNamespace {
-        val namespace = ConcretePythonInterpreter.getNewNamespace()
-        ConcretePythonInterpreter.concreteRun(namespace, program.asString)
-        return namespace
-    }
+    private val pinnedCallable = callable.reference(namespace)
 
     override fun step(state: PythonExecutionState): StepResult<PythonExecutionState> =
         with(ctx) {
             val symbols = state.inputSymbols
             val seeds = symbols.map { state.models.first().eval(it.expr) }
-            val namespace = prepareNamespace()
             val converter = ConverterToPythonObject(namespace)
-            val functionRef = callable.reference(namespace)
             val concrete = (seeds zip callable.signature).map { (seed, type) ->
                 converter.convert(seed, type) ?: error("Couldn't construct PythonObject from model")
             }
             val serializedInputs = concrete.map(pythonObjectSerialization)
             val concolicRunContext = ConcolicRunContext(state, ctx)
-            val result = ConcretePythonInterpreter.concolicRun(namespace, functionRef, concrete, symbols, concolicRunContext)
+            val result = ConcretePythonInterpreter.concolicRun(namespace, pinnedCallable, concrete, symbols, concolicRunContext)
             val serializedResult = pythonObjectSerialization(result)
             saveRunResult(
                 PythonAnalysisResult(
