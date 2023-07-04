6a4e8c70f1b4b2102d9e79acbeb6ed356feccd7d
Yury Kamenev <yurkam447@gmail.com>
2023-07-04 16:00:27 +0300

    Added matchers for mutations of this and params (#33)

--- usvm-jvm/src/test/kotlin/org/usvm/samples/mixed/PrivateConstructorExampleTest.kt
+++ usvm-jvm/src/test/kotlin/org/usvm/samples/mixed/PrivateConstructorExampleTest.kt
@@ -1,6 +1,8 @@
 package org.usvm.samples.mixed
 
+import org.junit.jupiter.api.Test
 import org.usvm.samples.JavaMethodTestRunner
+import org.usvm.test.util.checkers.eq
 
 
 internal class PrivateConstructorExampleTest : JavaMethodTestRunner() {
@@ -12,15 +14,14 @@ internal class PrivateConstructorExampleTest : JavaMethodTestRunner() {
      *
      * a and b are fields of the class under test
      */
-    // TODO unsupported matchers
-
-//    @Test
-//    fun testLimitedSub() {
-//        checkWithThis(
-//            PrivateConstructorExample::limitedSub,
-//            eq(2),
-//            { caller, limit, r -> caller.a - caller.b >= limit && r == caller.a - caller.b },
-//            { caller, limit, r -> caller.a - caller.b < limit && r == limit }, // TODO: Method coverage with `this` parameter isn't supported
-//        )
-//    }
+    @Test
+    fun testLimitedSub() {
+        checkThisAndParamsMutations(
+            PrivateConstructorExample::limitedSub,
+            eq(2),
+            { caller, limit, _, _, r -> caller.a - caller.b >= limit && r == caller.a - caller.b },
+            { caller, limit, _, _, r -> caller.a - caller.b < limit && r == limit }, // TODO: Method coverage with `this` parameter isn't supported
+            checkMode = CheckMode.MATCH_PROPERTIES
+        )
+    }
 }
