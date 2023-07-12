71cca364f69cbdf34a6404931cdd621638b44b93
DaniilStepanov <stepanov0995@mail.ru>
2023-07-12 17:22:57 +0300

    Global mock support

--- usvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/instrumentation/JcInstrumenter.kt
+++ usvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/instrumentation/JcInstrumenter.kt
@@ -1,9 +1,7 @@
 package org.usvm.instrumentation.instrumentation
 
-import org.jacodb.api.JcClassOrInterface
 import org.jacodb.api.JcClasspath
 import org.objectweb.asm.tree.ClassNode
-import org.usvm.instrumentation.trace.collector.TraceCollector
 
 interface JcInstrumenter {
 
