c7a6ef1e3e073815a948edb0b35fec05116a2964
DaniilStepanov <stepanov0995@mail.ru>
2023-07-12 17:22:57 +0300

    Global mock support

--- usvm-instrumentation/src/collectors/java/org/usvm/instrumentation/collector/trace/MockCollector.java
+++ usvm-instrumentation/src/collectors/java/org/usvm/instrumentation/collector/trace/MockCollector.java
@@ -1,35 +1,41 @@
-package org.usvm.instrumentation.trace.collector;
+package org.usvm.instrumentation.collector.trace;
 
 public class MockCollector {
 
     public static MockInfoArrayWrapper mocks = new MockInfoArrayWrapper();
 
+    public static boolean inExecution;
+
+    public static boolean isInExecution() {
+        return inExecution;
+    }
+
     public static Object getMockValue(long mockedId, Object mockedObject) {
-        return mocks.find(mockedId, mockedObject).mockValue;
+        return mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static boolean getBooleanMockValue(long mockedId, Object mockedObject) {
-        return (boolean) mocks.find(mockedId, mockedObject).mockValue;
+        return (boolean) mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static byte getByteMockValue(long mockedId, Object mockedObject) {
-        return (byte) mocks.find(mockedId, mockedObject).mockValue;
+        return (byte) mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static short getShortMockValue(long mockedId, Object mockedObject) {
-        return (short) mocks.find(mockedId, mockedObject).mockValue;
+        return (short) mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static char getCharMockValue(long mockedId, Object mockedObject) {
-        return (char) mocks.find(mockedId, mockedObject).mockValue;
+        return (char) mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static int getIntMockValue(long mockedId, Object mockedObject) {
-        return (int) mocks.find(mockedId, mockedObject).mockValue;
+        return (int) mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static float getFloatMockValue(long mockedId, Object mockedObject) {
-        return (float) mocks.find(mockedId, mockedObject).mockValue;
+        return (float) mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static double getDoubleMockValue(long mockedId, Object mockedObject) {
-        return (double) mocks.find(mockedId, mockedObject).mockValue;
+        return (double) mocks.find(mockedId, mockedObject).getMockValue();
     }
     public static long getLongMockValue(long mockedId, Object mockedObject) {
-        return (long) mocks.find(mockedId, mockedObject).mockValue;
+        return (long) mocks.find(mockedId, mockedObject).getMockValue();
     }
 
     public static boolean isMocked(long mockedId, Object mockedObject) {
@@ -46,12 +52,18 @@ public class MockCollector {
         public long mockedId;
 
         public Object mockedObject;
-        public Object mockValue;
+        public MockValueArrayWrapper mockValues;
 
-        public MockInfo(long mockedId, Object mockedObject, Object mockValue) {
+        private int refNumber = 0;
+
+        public MockInfo(long mockedId, Object mockedObject, MockValueArrayWrapper mockValues) {
             this.mockedId = mockedId;
             this.mockedObject = mockedObject;
-            this.mockValue = mockValue;
+            this.mockValues = mockValues;
+        }
+
+        public Object getMockValue() {
+            return mockValues.arr[refNumber++];
         }
 
     }
@@ -107,6 +119,49 @@ public class MockCollector {
         private void resize() {
             int newSize = (arr.length * 3) / 2;
             MockInfo[] newArr = new MockInfo[newSize];
+            for (int i = 0; i < arr.length; i++) {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+    }
+
+    public static class MockValueArrayWrapper {
+        public Object[] arr;
+        public int size;
+
+        public MockValueArrayWrapper() {
+            arr = new Object[10];
+        }
+
+        public MockValueArrayWrapper(int size) {
+            arr = new Object[size];
+        }
+
+        public void add(Object el) {
+            if (size >= arr.length) {
+                resize();
+            }
+            arr[size] = el;
+            size++;
+        }
+
+        public void removeLast() {
+            arr[size] = 0;
+            if (size == 0) return;
+            size--;
+        }
+
+        public void clear() {
+            for (int i = 0; i < size; i++) {
+                arr[i] = 0;
+            }
+            size = 0;
+        }
+
+        private void resize() {
+            int newSize = (arr.length * 3) / 2;
+            Object[] newArr = new Object[newSize];
             for (int i = 0; i < arr.length; i++) {
                 newArr[i] = arr[i];
             }
