9dddf147a8377acfc9dd0c0a67d72466e0c37a47
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-08-14 18:52:46 +0300

    small fixes

--- usvm-python/cpythonadapter/src/main/c/include/approximations.h
+++ usvm-python/cpythonadapter/src/main/c/include/approximations.h
@@ -1,5 +1,12 @@
 #include "Python.h"
 
+/* initializations of Python functions */
+void initialize_list_python_impls();
+
+#define INITIALIZE_PYTHON_APPROXIMATIONS \
+    initialize_list_python_impls();
+
+
 PyObject *Approximation_len(PyObject *o);  // builtins.len
 PyObject *Approximation_isinstance(PyObject *obj, PyObject *type);  // builtins.isinstance
 
