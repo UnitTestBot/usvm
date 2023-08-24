9e813a1e60dfd2641e8255bfac3c7a4c5e642d78
DaniilStepanov <stepanov0995@mail.ru>
2023-08-24 13:15:55 +0300

    minor

--- usvm-jvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/util/instrumentation.kt
+++ usvm-jvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/util/instrumentation.kt
@@ -68,10 +68,6 @@ data class JdkInfo(
 
 /**
  * Singleton to enable abstract access to path to JDK.
-
- * Used in [org.utbot.instrumentation.process.InstrumentedProcessRunner].
- * The purpose is to use the same JDK in [org.utbot.instrumentation.ConcreteExecutor] and in the test runs.
- * This is necessary because the engine can be run from the various starting points, like IDEA plugin, CLI, etc.
  */
 object JdkInfoService : PluginService<JdkInfo> {
     var jdkInfoProvider: JdkInfoProvider = JdkInfoDefaultProvider()
