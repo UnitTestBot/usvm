58f34cadd06a2e1220538f5aa455cacc92f7bba8
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-08-28 16:58:36 +0300

    Implemented list repeat and concat

--- usvm-python/cpythonadapter/src/main/c/include/approximations.h
+++ usvm-python/cpythonadapter/src/main/c/include/approximations.h
@@ -2,14 +2,18 @@
 
 /* initializations of Python functions */
 void initialize_list_python_impls();
+void initialize_builtin_python_impls();
 
 #define INITIALIZE_PYTHON_APPROXIMATIONS \
-    initialize_list_python_impls();
+    initialize_list_python_impls(); \
+    initialize_builtin_python_impls();
 
 
 PyObject *Approximation_len(PyObject *o);  // builtins.len
 PyObject *Approximation_isinstance(PyObject *obj, PyObject *type);  // builtins.isinstance
 PyObject *Approximation_range(void *adapter_raw, PyObject *args);  // builtins.range
+PyObject *Approximation_sum(PyObject *iterable);  // builtins.sum
 
 PyObject *Approximation_list_richcompare(PyObject *, PyObject *, int op);  // PyList_Type.tp_richcompare
 PyObject *Approximation_list_append(PyObject *append_method, PyObject *symbolic_list, PyObject *wrapped_elem);  // list.append
+PyObject *Approximation_list_repeat(PyObject *self, PyObject *n);  // PyList_Type.tp_as_sequence.sq_repeat
