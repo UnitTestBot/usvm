90283ebec13da48873f492aef2c7b19b81aaa076
tochilinak <tochilina.2002@yandex.ru>
2023-09-14 13:29:49 +0300

    slice fields

--- usvm-python/src/test/kotlin/org/usvm/samples/SlicesTest.kt
+++ usvm-python/src/test/kotlin/org/usvm/samples/SlicesTest.kt
@@ -12,12 +12,50 @@ class SlicesTest: PythonTestRunnerForStructuredProgram("Slices", UMachineOptions
         check1WithConcreteRun(
             constructFunction("field_start", List(1) { PythonAnyType }),
             ignoreNumberOfAnalysisResults,
-            compareConcolicAndConcreteTypes,
+            standardConcolicAndConcreteChecks,
             /* invariants = */ emptyList(),
             /* propertiesToDiscover = */ listOf(
                 { x, res -> x.typeName == "slice" && res.selfTypeName == "AssertionError" },
                 { x, res -> x.typeName == "slice" && res.repr == "None" }
             )
+        )
+    }
+    @Test
+    fun testFieldStop() {
+        check1WithConcreteRun(
+            constructFunction("field_stop", List(1) { PythonAnyType }),
+            ignoreNumberOfAnalysisResults,
+            standardConcolicAndConcreteChecks,
+            /* invariants = */ emptyList(),
+            /* propertiesToDiscover = */ listOf(
+                { x, res -> x.typeName == "slice" && res.selfTypeName == "AssertionError" },
+                { x, res -> x.typeName == "slice" && res.repr == "None" }
+            )
+        )
+    }
+    @Test
+    fun testFieldStep() {
+        check1WithConcreteRun(
+            constructFunction("field_step", List(1) { PythonAnyType }),
+            ignoreNumberOfAnalysisResults,
+            standardConcolicAndConcreteChecks,
+            /* invariants = */ emptyList(),
+            /* propertiesToDiscover = */ listOf(
+                { x, res -> x.typeName == "slice" && res.selfTypeName == "AssertionError" },
+                { x, res -> x.typeName == "slice" && res.repr == "None" }
+            )
+        )
+    }
+    @Test
+    fun testNoneFields() {
+        check1WithConcreteRun(
+            constructFunction("none_fields", List(1) { PythonAnyType }),
+            ignoreNumberOfAnalysisResults,
+            standardConcolicAndConcreteChecks,
+            /* invariants = */ emptyList(),
+            /* propertiesToDiscover = */ List(6) {
+                { x, res -> x.typeName == "slice" && res.repr == (it + 1).toString() }
+            }
         )
     }
 }
