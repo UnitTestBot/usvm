469e95aaa0556c65e142f70ed6068362b631ba8a
tochilinak <tochilina.2002@yandex.ru>
2023-09-20 11:49:19 +0300

    Fixes after rebase

--- usvm-python/src/main/kotlin/org/usvm/machine/UPythonContext.kt
+++ usvm-python/src/main/kotlin/org/usvm/machine/UPythonContext.kt
@@ -1,13 +1,18 @@
 package org.usvm.machine
 
+import org.usvm.INITIAL_STATIC_ADDRESS
 import org.usvm.UConcreteHeapAddress
 import org.usvm.UConcreteHeapRef
 import org.usvm.UContext
 import org.usvm.language.types.PythonTypeSystem
 
 class UPythonContext(typeSystem: PythonTypeSystem): UContext(PythonComponents(typeSystem)) {
-    private var nextAddress: UConcreteHeapAddress = -1_000_000_000
+    private var nextAddress: UConcreteHeapAddress = INITIAL_STATIC_ADDRESS / 2
     fun provideRawConcreteHeapRef(): UConcreteHeapRef {
+        require(nextAddress > INITIAL_STATIC_ADDRESS) {
+            "Should not return a static ref"
+        }
+
         return mkConcreteHeapRef(nextAddress--)
     }
 }
