6223157e1a55d85cd1e012528f37ad2be40a1805
tochilinak <tochilina.2002@yandex.ru>
2023-09-28 15:16:07 +0300

    Fixed memory leak from PathTrieNode

--- usvm-python/src/main/kotlin/org/usvm/machine/UPythonContext.kt
+++ usvm-python/src/main/kotlin/org/usvm/machine/UPythonContext.kt
@@ -5,9 +5,14 @@ import io.ksmt.sort.KFp64Sort
 import io.ksmt.sort.KIntSort
 import io.ksmt.sort.KRealSort
 import org.usvm.*
+import org.usvm.language.types.PythonType
 import org.usvm.language.types.PythonTypeSystem
+import org.usvm.solver.USolverBase
 
-class UPythonContext(typeSystem: PythonTypeSystem): UContext(PythonComponents(typeSystem)) {
+class UPythonContext(
+    typeSystem: PythonTypeSystem,
+    private val components: PythonComponents = PythonComponents(typeSystem)
+): UContext(components) {
     private var nextAddress: UConcreteHeapAddress = INITIAL_STATIC_ADDRESS / 2
     fun provideRawConcreteHeapRef(): UConcreteHeapRef {
         require(nextAddress > INITIAL_STATIC_ADDRESS) {
@@ -22,5 +27,21 @@ class UPythonContext(typeSystem: PythonTypeSystem): UContext(PythonComponents(ty
     fun intToFloat(intValue: UExpr<KIntSort>): UExpr<KRealSort> {
         return mkIntToReal(intValue)
         //return mkRealToFpExpr(fp64Sort, floatRoundingMode, realValue)
+    }
+
+
+    private var solver: USolverBase<PythonType, UPythonContext> = components.mkSolver(this)
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <Type, Context : UContext> solver(): USolverBase<Type, Context> =
+        solver as USolverBase<Type, Context>
+
+    fun restartSolver() {
+        solver.close()
+        solver = components.mkSolver(this)
+    }
+
+    fun closeSolver() {
+        solver.close()
     }
 }
