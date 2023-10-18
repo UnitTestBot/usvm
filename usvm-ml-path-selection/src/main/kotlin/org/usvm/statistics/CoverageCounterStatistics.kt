f837ef3ab5cfafad50c29ce6fe68236bcfaef09e
sergeyrid <sergeyrid@yandex.ru>
2023-10-19 02:03:28 +0300

    Refactor CoverageCounterStatistics

--- usvm-ml-path-selection/src/main/kotlin/org/usvm/statistics/CoverageCounterStatistics.kt
+++ usvm-ml-path-selection/src/main/kotlin/org/usvm/statistics/CoverageCounterStatistics.kt
@@ -6,23 +6,23 @@ import org.usvm.UState
 class CoverageCounterStatistics<State : UState<*, *, *, *, *, State>>(
     private val coverageStatistics: CoverageStatistics<*, *, State>,
     private val coverageCounter: CoverageCounter,
-    private val methodName: String
+    private val methodFullName: String
 ) : UMachineObserver<State> {
     private val totalStatementsCount = coverageStatistics.getUncoveredStatements().size
     private var totalCoverage = 0
 
     init {
-        coverageCounter.addTest(methodName, totalStatementsCount.toFloat())
+        coverageCounter.addTest(methodFullName, totalStatementsCount.toFloat())
     }
 
     override fun onState(parent: State, forks: Sequence<State>) {
-        coverageCounter.updateDiscounts(methodName)
+        coverageCounter.updateDiscounts(methodFullName)
     }
 
     override fun onStateTerminated(state: State, stateReachable: Boolean) {
         if (!stateReachable) return
         val newTotalCoverage = totalStatementsCount - coverageStatistics.getUncoveredStatements().size
-        coverageCounter.updateResults(methodName, (newTotalCoverage - totalCoverage).toFloat())
+        coverageCounter.updateResults(methodFullName, (newTotalCoverage - totalCoverage).toFloat())
         totalCoverage = newTotalCoverage
     }
 }
