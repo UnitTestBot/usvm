ffdedd8ceac73e6634e3286bb3c2570d60d86876
tochilinak <tochilina.ekaterina@huawei-partners.com>
2023-10-31 16:25:58 +0300

    work on usvm-python package

---
+++ usvm-python/buildDistWithoutPip.sh
@@ -0,0 +1,6 @@
+../gradlew :usvm-python:cpythonadapter:CPythonClean
+../gradlew :usvm-python:cpythonadapter:CPythonDistclean
+../gradlew :usvm-python:clean
+../gradlew :usvm-python:cpythonadapter:CPythonBuildWithoutPip
+../gradlew :usvm-python:jar
+../gradlew :usvm-python:distZip
