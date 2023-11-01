a5027511e3f69c42a932bcccc759a1fdb236f9d4
tochilinak <tochilina.ekaterina@huawei-partners.com>
2023-11-01 16:23:48 +0300

    Added debug runner

---
+++ usvm-python/usvm-python-runner/src/main/kotlin/org/usvm/runner/USVMPythonRunner.kt
@@ -0,0 +1,52 @@
+package org.usvm.runner
+
+import java.io.File
+import java.net.InetSocketAddress
+import java.nio.channels.ServerSocketChannel
+
+open class USVMPythonRunner(private val config: USVMPythonConfig): AutoCloseable {
+    protected val serverSocketChannel: ServerSocketChannel = ServerSocketChannel.open()
+    private val port: InetSocketAddress
+
+    init {
+        serverSocketChannel.socket().bind(InetSocketAddress(0))
+        port = serverSocketChannel.localAddress as? InetSocketAddress
+            ?: error("Couldn't cast SocketAddress ${serverSocketChannel.localAddress} to InetSocketAddress")
+        println("Port: ${port.port}")
+    }
+
+    override fun close() {
+        serverSocketChannel.close()
+    }
+
+    protected fun setupEnvironment(runConfig: USVMPythonRunConfig): ProcessBuilder {
+        val layout = config.distributionLayout
+        val functionConfig = when (runConfig.callableConfig) {
+            is USVMPythonFunctionConfig -> runConfig.callableConfig
+        }
+        val args = listOf(
+            config.javaCmd,
+            "-Xss50m",
+            "-Xmx2g",
+            "-Dapproximations.path=${layout.approximationsPath.canonicalPath}",
+            "-Djava.library.path=${layout.nativeLibPath.canonicalPath}",
+            "-jar",
+            layout.jarPath.canonicalPath,
+            config.mypyBuildDir,
+            port.port.toString(),
+            functionConfig.module,
+            functionConfig.name,
+            runConfig.timeoutPerRunMs.toString(),
+            runConfig.timeoutMs.toString()
+        ) + config.roots.toList()
+
+        val processBuilder = ProcessBuilder(args)
+
+        val env = processBuilder.environment()
+        env["LD_LIBRARY_PATH"] = "${File(layout.cpythonPath, "lib").canonicalPath}:${layout.cpythonPath.canonicalPath}"
+        env["LD_PRELOAD"] = File(layout.cpythonPath, "lib/libpython3.so").canonicalPath
+        env["PYTHONHOME"] = layout.cpythonPath.canonicalPath
+
+        return processBuilder
+    }
+}
