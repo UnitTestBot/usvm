359f29cd132b4dfd00ac90b3c5883fb564a27620
Egor Kulikov <egor.k.kulikov@gmail.com>
2023-11-01 14:09:58 +0300

    Correct after rebase

--- usvm-core/src/main/kotlin/org/usvm/statistics/constraints/SoftConstraintsObserver.kt
+++ usvm-core/src/main/kotlin/org/usvm/statistics/constraints/SoftConstraintsObserver.kt
@@ -3,9 +3,10 @@ package org.usvm.statistics.constraints
 import org.usvm.UState
 import org.usvm.utils.applySoftConstraints
 import org.usvm.statistics.UMachineObserver
+import java.util.concurrent.atomic.AtomicBoolean
 
 class SoftConstraintsObserver<Type, State : UState<Type, *, *, *, *, State>> : UMachineObserver<State> {
-    override fun onStateTerminated(state: State, stateReachable: Boolean) {
+    override fun onStateTerminated(state: State, stateReachable: Boolean, isConsumed: AtomicBoolean) {
         if (stateReachable) {
             // TODO actually, only states presented in CoveredNewStatesCollector should be here,
             //  so for now soft constraints are applied for more states than required. Rewrite it after refactoring
