14e04db93132106c85b4097fd50f6563fa23fe02
Yury Kamenev <yurkam447@gmail.com>
2023-11-03 11:43:09 +0300

    Added prettifer for executions

--- usvm-jvm/src/main/kotlin/org/usvm/api/JcTest.kt
+++ usvm-jvm/src/main/kotlin/org/usvm/api/JcTest.kt
@@ -3,6 +3,7 @@ package org.usvm.api
 import org.jacodb.api.JcClassOrInterface
 import org.jacodb.api.JcTypedMethod
 import org.jacodb.api.cfg.JcInst
+import org.usvm.api.util.prettify
 
 // Reflection-based API
 
@@ -16,17 +17,27 @@ data class JcTest(
     val after: JcParametersState,
     val result: Result<Any?>,
     val coverage: JcCoverage,
-)
+) {
+    override fun toString(): String =
+        // TODO use fully qualified name of the `method` instead of `toString`
+        "JcTest(method=$method, before=$before, after=$after, result=$result, coverage=$coverage)"
+}
 
 data class JcParametersState(
     val thisInstance: Any?,
     val parameters: List<Any?>,
-)
+) {
+    override fun toString(): String = "(this=${thisInstance.prettify()}, params=${parameters.prettify()})"
+}
 
 data class JcCoverage(
     val targetClassToCoverage: Map<JcClassOrInterface, JcClassCoverage>,
-)
+) {
+    override fun toString(): String = "(${targetClassToCoverage.prettify()})"
+}
 
 data class JcClassCoverage(
     val visitedStmts: Set<JcInst>,
-)
+) {
+    override fun toString(): String = "(${visitedStmts.prettify()})"
+}
---
+++ usvm-jvm/src/main/kotlin/org/usvm/api/util/Prettifier.kt
@@ -0,0 +1,25 @@
+package org.usvm.api.util
+
+@Suppress("UNCHECKED_CAST")
+// NOTE: this prettifier DOES NOT cover all the possible cases
+fun Any?.prettify(): String {
+    return when {
+        this == null -> toString()
+        this::class.java.isArray -> {
+            when (this::class.java.componentType) {
+                Long::class.java -> (this as LongArray).contentToString()
+                Int::class.java -> (this as IntArray).contentToString()
+                Char::class.java -> (this as CharArray).contentToString()
+                Short::class.java -> (this as ShortArray).contentToString()
+                Byte::class.java -> (this as ByteArray).contentToString()
+                Double::class.java -> (this as DoubleArray).contentToString()
+                Float::class.java -> (this as FloatArray).contentToString()
+                Boolean::class.java -> (this as BooleanArray).contentToString()
+                else -> (this as Array<out Any>).contentDeepToString()
+            }
+        }
+        this is Collection<*> -> joinToString(prefix = "[", postfix = "]") { it.prettify() }
+        this is Map<*, *> -> entries.joinToString(prefix = "{", postfix = "}") { "${it.key.prettify()}: ${it.value.prettify()}" }
+        else -> toString()
+    }
+}
