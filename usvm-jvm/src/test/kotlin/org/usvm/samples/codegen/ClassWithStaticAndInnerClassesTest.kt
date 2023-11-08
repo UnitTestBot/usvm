742e0d83e220c0ecfe27c777255d55425a8cd468
Yury Kamenev <yurkam447@gmail.com>
2023-11-08 12:50:14 +0300

    Enabled `org.usvm.samples.codegen.ClassWithStaticAndInnerClassesTest`

--- usvm-jvm/src/test/kotlin/org/usvm/samples/codegen/ClassWithStaticAndInnerClassesTest.kt
+++ usvm-jvm/src/test/kotlin/org/usvm/samples/codegen/ClassWithStaticAndInnerClassesTest.kt
@@ -1,14 +1,12 @@
 package org.usvm.samples.codegen
 
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.usvm.samples.JavaMethodTestRunner
 import org.usvm.test.util.checkers.eq
 
 
 @Suppress("INACCESSIBLE_TYPE")
-@Disabled("Expected exactly 2 executions, but 1 found. Fix inner classes")
 internal class ClassWithStaticAndInnerClassesTest : JavaMethodTestRunner() {
     @Test
     fun testUsePrivateStaticClassWithPrivateField() {
