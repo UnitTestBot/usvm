032e948d0e2e2117e078d793a15b393ab6cbb185
Yury Kamenev <yurkam447@gmail.com>
2023-11-08 16:27:03 +0300

    Enabled `org.usvm.samples.mixed.SimplifierTest`

--- usvm-jvm/src/test/kotlin/org/usvm/samples/mixed/SimplifierTest.kt
+++ usvm-jvm/src/test/kotlin/org/usvm/samples/mixed/SimplifierTest.kt
@@ -1,6 +1,5 @@
 package org.usvm.samples.mixed
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.usvm.samples.JavaMethodTestRunner
 import org.usvm.test.util.checkers.eq
@@ -8,7 +7,6 @@ import org.usvm.test.util.checkers.eq
 
 internal class SimplifierTest: JavaMethodTestRunner() {
     @Test
-    @Disabled("Support assumptions")
     fun testSimplifyAdditionWithZero() {
         checkDiscoveredProperties(
             Simplifier::simplifyAdditionWithZero,
