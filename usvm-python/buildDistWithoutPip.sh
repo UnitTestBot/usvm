70b0715ba864f676710e083fafad40d9cdc91c12
tochilinak <tochilina.ekaterina@huawei-partners.com>
2023-11-09 15:27:42 +0300

    fixed compilation on linux

--- usvm-python/buildDistWithoutPip.sh
+++ usvm-python/buildDistWithoutPip.sh
@@ -1,10 +1,10 @@
-../gradlew :usvm-python:cpythonadapter:CPythonClean
-../gradlew :usvm-python:cpythonadapter:CPythonDistclean
-../gradlew :usvm-python:cpythonadapter:clean
-../gradlew :usvm-python:clean
-echo "false" > cpythonadapter/include_pip_in_build
-../gradlew :usvm-python:cpythonadapter:linkDebug
-../gradlew :usvm-core:build
-../gradlew :usvm-python:jar
-../gradlew :usvm-python:distZip
+../gradlew :usvm-python:cpythonadapter:CPythonClean &&\
+../gradlew :usvm-python:cpythonadapter:CPythonDistclean &&\
+../gradlew :usvm-python:cpythonadapter:clean &&\
+../gradlew :usvm-python:clean &&\
+echo "false" > cpythonadapter/include_pip_in_build &&\
+../gradlew :usvm-python:cpythonadapter:linkDebug &&\
+../gradlew :usvm-core:jar &&\
+../gradlew :usvm-python:jar &&\
+../gradlew :usvm-python:distZip &&\
 echo "true" > cpythonadapter/include_pip_in_build
