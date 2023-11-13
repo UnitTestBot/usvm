556363372b9a7b06e8f7ce63533179b5a82bcffa
IlyaMuravjov <muravjovilya@gmail.com>
2023-11-13 16:10:18 +0300

    Store `enumValue.name` in `enumValueName` (not `enumValue.toString()`)

--- usvm-jvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/testcase/descriptor/Value2DescriptorConverter.kt
+++ usvm-jvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/testcase/descriptor/Value2DescriptorConverter.kt
@@ -205,11 +205,11 @@ open class Value2DescriptorConverter(
         )
     }
 
-    private fun `enum`(value: Any, depth: Int): UTestEnumValueDescriptor {
+    private fun `enum`(value: Enum<*>, depth: Int): UTestEnumValueDescriptor {
         val enumValueJcClass = jcClasspath.findClass(value::class.java.name)
         val jcClass = if (!enumValueJcClass.isEnum) enumValueJcClass.superClass!! else enumValueJcClass
         val fields = mutableMapOf<JcField, UTestValueDescriptor>()
-        val enumValueName = value.toString()
+        val enumValueName = value.name
         val jcType = jcClass.toType()
         val uTestEnumValueDescriptor =
             UTestEnumValueDescriptor(jcType, enumValueName, fields, System.identityHashCode(value))
