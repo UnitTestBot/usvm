0b00f36848d359528635f72848b93ed19cf6d7c3
Yury Kamenev <yurkam447@gmail.com>
2023-11-13 17:34:50 +0300

    Fixed compilation after rebasing

--- usvm-jvm/src/test/kotlin/org/usvm/samples/controlflow/CyclesTest.kt
+++ usvm-jvm/src/test/kotlin/org/usvm/samples/controlflow/CyclesTest.kt
@@ -1,5 +1,6 @@
 package org.usvm.samples.controlflow
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.usvm.PathSelectionStrategy
 import org.usvm.UMachineOptions
