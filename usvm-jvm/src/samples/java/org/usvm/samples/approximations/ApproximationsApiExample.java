057c86bc867d3360a6edd1cf8237d5b8b1d476bd
Valentyn Sobol <napster_1997@mail.ru>
2023-11-17 16:46:02 +0300

    map test

--- usvm-jvm/src/samples/java/org/usvm/samples/approximations/ApproximationsApiExample.java
+++ usvm-jvm/src/samples/java/org/usvm/samples/approximations/ApproximationsApiExample.java
@@ -16,4 +16,49 @@ public class ApproximationsApiExample {
 
         return 3;
     }
+
+    public static int symbolicMap(TestMap<String, Integer> map) {
+        if (map.size() < 10) {
+            return 0;
+        }
+
+        if (!map.containsKey("abc")) {
+            return 1;
+        }
+
+        int value = map.get("abc");
+        if (value != 5) {
+            return 2;
+        }
+
+        TestMap<String, Integer> other = new TestMap<>();
+        other.put("abc", 7);
+        other.put("xxx", 17);
+        other.putAll(map);
+
+
+        if (!map.containsKey("abc")) {
+            // unreachable
+            return 3;
+        }
+
+        value = map.get("abc");
+        if (value != 5) {
+            // unreachable
+            return 4;
+        }
+
+        if (!map.containsKey("xxx")) {
+            // unreachable
+            return 5;
+        }
+
+        value = map.get("xxx");
+
+        if (value == 17) {
+            return 6;
+        } else {
+            return 7;
+        }
+    }
 }
