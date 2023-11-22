bf8e1e61ab71c0dd6bcbf8b715a93baa8cd72c75
Valentyn Sobol <napster_1997@mail.ru>
2023-11-22 10:56:12 +0300

    Flow state collectors (#125)
    * Avoid `jdk.internal` in `org.usvm.instrumentation.util.URLClassPathLoader` (#94)
    
    * Flow collectors
    
    ---------
    
    Co-authored-by: IlyaMuravjov <71839386+IlyaMuravjov@users.noreply.github.com>

--- usvm-core/src/main/kotlin/org/usvm/statistics/collectors/TargetsReachedStatesCollector.kt
+++ usvm-core/src/main/kotlin/org/usvm/statistics/collectors/TargetsReachedStatesCollector.kt
@@ -1,20 +1,20 @@
 package org.usvm.statistics.collectors
 
 import org.usvm.UState
+import org.usvm.statistics.UMachineObserver
 
 /**
  * [StatesCollector] implementation collecting only those states which have reached
  * any terminal targets.
  */
-class TargetsReachedStatesCollector<State : UState<*, *, *, *, *, State>> : StatesCollector<State> {
-    private val mutableCollectedStates = mutableListOf<State>()
-    override val collectedStates: List<State> = mutableCollectedStates
-
+class TargetsReachedStatesCollector<State : UState<*, *, *, *, *, State>>(
+    val statesCollector: StatesCollector<State>
+) : UMachineObserver<State> {
     // TODO probably this should be called not only for terminated states
     //      Also, we should process more carefully clone operation for the states
     override fun onStateTerminated(state: State, stateReachable: Boolean) {
         if (state.targets.reachedTerminal.isNotEmpty()) {
-            mutableCollectedStates.add(state)
+            statesCollector.addState(state)
         }
     }
 }
