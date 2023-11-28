daf9e37d21ddecc84a83411f392d8853446a5fee
tochilinak <tochilina.ekaterina@huawei-partners.com>
2023-11-28 19:44:02 +0300

    Added substitution of virtual objects to None

--- usvm-python/usvm-python-runner/src/test/kotlin/org/usvm/runner/PrintingResultReceiver.kt
+++ usvm-python/usvm-python-runner/src/test/kotlin/org/usvm/runner/PrintingResultReceiver.kt
@@ -1,7 +1,9 @@
 package org.usvm.runner
 
 class PrintingResultReceiver: USVMPythonAnalysisResultReceiver() {
+    var cnt: Int = 0
     override fun receivePickledInputValues(pickledTuple: String) {
         println(pickledTuple)
+        cnt += 1
     }
 }
