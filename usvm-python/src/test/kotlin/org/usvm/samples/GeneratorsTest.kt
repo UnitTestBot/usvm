5a6fb3d11e32a8d4a888e63acb2432d16369ca11
tochilinak <tochilina.2002@yandex.ru>
2023-11-28 17:11:08 +0300

    Added support for generators

--- usvm-python/src/test/kotlin/org/usvm/samples/GeneratorsTest.kt
+++ usvm-python/src/test/kotlin/org/usvm/samples/GeneratorsTest.kt
@@ -3,6 +3,7 @@ package org.usvm.samples
 import org.junit.jupiter.api.Test
 import org.usvm.runner.PythonTestRunnerForPrimitiveProgram
 import org.usvm.test.util.checkers.eq
+import org.usvm.test.util.checkers.ignoreNumberOfAnalysisResults
 
 class GeneratorsTest : PythonTestRunnerForPrimitiveProgram("Generators") {
     @Test
@@ -16,6 +17,20 @@ class GeneratorsTest : PythonTestRunnerForPrimitiveProgram("Generators") {
                 { _, res -> res.repr == "'one'" },
                 { _, res -> res.repr == "'two'" },
                 { _, res -> res.repr == "'other'" }
+            )
+        )
+    }
+
+    @Test
+    fun testSimpleGenerator() {
+        check1(
+            constructFunction("simple_generator", listOf(typeSystem.pythonInt)),
+            ignoreNumberOfAnalysisResults,
+            /* invariants = */ emptyList(),
+            /* propertiesToDiscover = */ listOf(
+                { _, res -> res.repr == "['one']" },
+                { _, res -> res.repr == "['two']" },
+                { _, res -> res.repr == "['other']" }
             )
         )
     }
