0d365a0b32d336ab14f568de478bf8d38bda7447
Yury Kamenev <yurkam447@gmail.com>
2023-12-05 15:14:08 +0300

    Fixed classloader for logger

--- usvm-jvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/classloader/WorkerClassLoader.kt
+++ usvm-jvm-instrumentation/src/main/kotlin/org/usvm/instrumentation/classloader/WorkerClassLoader.kt
@@ -89,7 +89,12 @@ class WorkerClassLoader(
             super.loadClass(name)
         } catch (e: Throwable) {
             //In case of jdk classes which are not in Bootstrap classloader
-            traceCollectorClassLoader.loadClass(name)
+            val cls = traceCollectorClassLoader.loadClass(name)
+            if (cls.classLoader != null) {
+                throw e
+            }
+
+            cls
         }
     }
 
@@ -103,7 +108,7 @@ class WorkerClassLoader(
 
     override fun findClass(name: String): Class<*> {
         return foundClasses.getOrPut(name) {
-            val res = getWorkerResource(name)
+            val res = getWorkerResource(name) ?: throw ClassNotFoundException(name)
             val bb = res.getBytes()
             val cs = CodeSource(res.getCodeSourceURL(), res.getCodeSigners())
             val foundClass = defineClass(name, bb, 0, bb.size, cs)
@@ -120,9 +125,9 @@ class WorkerClassLoader(
         }
     }
 
-    private fun getWorkerResource(name: String): URLClassPathLoader.Resource = cachedClasses.getOrPut(name) {
+    private fun getWorkerResource(name: String): URLClassPathLoader.Resource? = cachedClasses.getOrPut(name) {
         val path = name.replace('.', '/') + ".class"
-        val resource = urlClassPath.getResource(path)
+        val resource = urlClassPath.getResource(path) ?: return null
         WorkerResource(resource)
     }
 
