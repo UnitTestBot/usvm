d4c967b6f44712f9d843e0c165d20c4f13de07f3
Emil <buraindo24@gmail.com>
2023-12-27 17:07:20 +0300

    added all unary/binary ops, bv/fp/bool types

--- usvm-go/src/main/go/usvm/interpreter/interpreter.go
+++ usvm-go/src/main/go/usvm/interpreter/interpreter.go
@@ -158,133 +158,79 @@ func visit(api api.Api, instr ssa.Instruction) continuation {
 		// no-op
 
 	case *ssa.UnOp:
-		log.Println("UnOp")
+		api.MkUnOp(inst)
 
 	case *ssa.BinOp:
-		api.Log("BinOp", inst.X.Name(), inst.Op.String(), inst.Y.Name(), api.GetLastBlock())
-
 		api.MkBinOp(inst)
 
 	case *ssa.Call:
-		log.Println("Call")
 
 	case *ssa.ChangeInterface:
-		log.Println("ChangeInterface")
 
 	case *ssa.ChangeType:
-		log.Println("ChangeType")
 
 	case *ssa.Convert:
-		log.Println("Convert")
 
 	case *ssa.SliceToArrayPointer:
-		log.Println("SliceToArrayPointer")
 
 	case *ssa.MakeInterface:
-		log.Println("MakeInterface")
 
 	case *ssa.Extract:
-		log.Println("Extract")
 
 	case *ssa.Slice:
-		log.Println("Slice")
 
 	case *ssa.Return:
-		api.Log("Return", inst.Parent(), inst.Results)
-
-		switch len(inst.Results) {
-		case 0:
-		case 1:
-			api.MkReturn(inst.Results[0])
-		default:
-		}
+		api.MkReturn(inst)
 		return kReturn
 
 	case *ssa.RunDefers:
-		log.Println("RunDefers")
 
 	case *ssa.Panic:
-		log.Println("Panic")
 
 	case *ssa.Send:
-		log.Println("Send")
 
 	case *ssa.Store:
-		log.Println("Store")
 
 	case *ssa.If:
-		api.Log("If",
-			inst.String(),
-			inst.Cond.String(),
-			inst.Block().Succs[0].Instrs[0],
-			inst.Block().Succs[1].Instrs[0],
-		)
-
 		api.MkIf(inst)
 		return kReturn
 
 	case *ssa.Jump:
-		api.Log("Jump", inst, inst.Block().Index, api.GetLastBlock())
-
 		return kJump
 
 	case *ssa.Defer:
-		log.Println("Defer")
 
 	case *ssa.Go:
-		log.Println("Go")
 
 	case *ssa.MakeChan:
-		log.Println("MakeChan")
 
 	case *ssa.Alloc:
-		log.Println("Alloc")
 
 	case *ssa.MakeSlice:
-		log.Println("MakeSlice")
 
 	case *ssa.MakeMap:
-		log.Println("MakeMap")
 
 	case *ssa.Range:
-		log.Println("Range")
 
 	case *ssa.Next:
-		log.Println("Next")
 
 	case *ssa.FieldAddr:
-		log.Println("FieldAddr")
 
 	case *ssa.Field:
-		log.Println("Field")
 
 	case *ssa.IndexAddr:
-		log.Println("IndexAddr")
 
 	case *ssa.Index:
-		log.Println("Index")
 
 	case *ssa.Lookup:
-		log.Println("Lookup")
 
 	case *ssa.MapUpdate:
-		log.Println("MapUpdate")
 
 	case *ssa.TypeAssert:
-		log.Println("TypeAssert")
 
 	case *ssa.MakeClosure:
-		log.Println("MakeClosure")
 
 	case *ssa.Phi:
-		api.Log("Phi",
-			inst.Name(),
-			inst.String(),
-			inst.Edges,
-			inst.Comment,
-			api.GetLastBlock(),
-		)
-
 		var edge ssa.Value
 		block := api.GetLastBlock()
 		for i, pred := range inst.Block().Preds {
@@ -297,7 +243,6 @@ func visit(api api.Api, instr ssa.Instruction) continuation {
 		api.MkVariable(inst.Name(), edge)
 
 	case *ssa.Select:
-		log.Println("Select")
 
 	default:
 		panic(fmt.Sprintf("unexpected instruction: %T", inst))
---
+++ usvm-go/src/main/go/usvm/ssadump.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"usvm/interpreter"
+)
+
+func main() {
+	dir := "C:/Users/burai/Documents/University/MastersDiploma/expr/programs/usvm"
+	switch runtime.GOOS {
+	case "darwin":
+		dir = "/Users/e.k.ibragimov/Documents/University/MastersDiploma/programs"
+	case "linux":
+		dir = "/home/buraindo/programs"
+	}
+
+	_, err := interpreter.NewInterpreter(fmt.Sprintf("%s/struct.go", dir), interpreter.Config{
+		DumpSsa: true,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
