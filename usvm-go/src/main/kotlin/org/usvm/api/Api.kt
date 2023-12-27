c7524bcfc4fb0744ea8acad3eacc013e511a4c12
Emil <buraindo24@gmail.com>
2023-12-27 17:07:20 +0300

    added all unary/binary ops, bv/fp/bool types

--- usvm-go/src/main/kotlin/org/usvm/api/Api.kt
+++ usvm-go/src/main/kotlin/org/usvm/api/Api.kt
@@ -1,6 +1,7 @@
 package org.usvm.api
 
 import io.ksmt.utils.asExpr
+import org.usvm.UBvSort
 import org.usvm.UExpr
 import org.usvm.USort
 import org.usvm.machine.GoContext
@@ -15,6 +16,7 @@ class Api(
     fun mk(args: ByteArray, setLastBlock: Boolean) {
         val buf = ByteBuffer.wrap(args)
         when (Method.valueOf(buf.get())) {
+            Method.MK_UN_OP -> mkUnOp(buf)
             Method.MK_BIN_OP -> mkBinOp(buf)
             Method.MK_IF -> mkIf(buf)
             Method.MK_RETURN -> mkReturn(buf)
@@ -35,90 +37,122 @@ class Api(
         scope.doWithState { lastBlock = block }
     }
 
-    private fun mkBinOp(buf: ByteBuffer) {
-        val op = BinOp.valueOf(buf.get())
+    private fun mkUnOp(buf: ByteBuffer) {
+        val op = UnOp.valueOf(buf.get())
+        val sort = toSort(Type.valueOf(buf.get()))
         val idx = resolveIndex(VarKind.LOCAL, buf.int)
-        val fst = resolveVar(ctx.bv32Sort, VarKind.valueOf(buf.get()), buf.int)
-        val snd = resolveVar(ctx.bv32Sort, VarKind.valueOf(buf.get()), buf.int)
+        val x = resolveVar(buf)
 
-        if (op == BinOp.ILLEGAL || fst == null || snd == null) {
+        if (op == UnOp.ILLEGAL || sort == null || x == null) {
             return
         }
 
-        var outSort: USort = ctx.bv32Sort
         val expr = when (op) {
-            BinOp.EQ -> {
-                outSort = ctx.boolSort
-                ctx.mkEq(fst, snd)
+            UnOp.RECV -> TODO()
+            UnOp.NEG -> ctx.mkBvNegationExpr(bv(x))
+            UnOp.DEREF -> TODO()
+            UnOp.NOT -> ctx.mkNot(x.asExpr(ctx.boolSort))
+            UnOp.INV -> ctx.mkBvNotExpr(bv(x))
+            else -> null
+        } ?: return
+
+        val lvalue = URegisterStackLValue(sort, idx)
+        scope.doWithState {
+            memory.write(lvalue, expr.asExpr(sort), ctx.trueExpr)
+        }
+    }
+
+    private fun mkBinOp(buf: ByteBuffer) {
+        val op = BinOp.valueOf(buf.get())
+        val type = Type.valueOf(buf.get())
+        val signed = type.isSigned()
+        val sort = toSort(type)
+        val idx = resolveIndex(VarKind.LOCAL, buf.int)
+        val x = resolveVar(buf)
+        val y = resolveVar(buf)
+
+        if (op == BinOp.ILLEGAL || sort == null || x == null || y == null) {
+            return
+        }
+
+        val expr = when (op) {
+            BinOp.ADD -> ctx.mkBvAddExpr(bv(x), bv(y))
+            BinOp.SUB -> ctx.mkBvSubExpr(bv(x), bv(y))
+            BinOp.MUL -> ctx.mkBvMulExpr(bv(x), bv(y))
+            BinOp.DIV -> {
+                val l = bv(x)
+                val r = bv(y)
+                if (signed) {
+                    ctx.mkBvSignedDivExpr(l, r)
+                } else {
+                    ctx.mkBvUnsignedDivExpr(l, r)
+                }
             }
 
-            BinOp.NEQ -> {
-                outSort = ctx.boolSort
-                ctx.mkNot(ctx.mkEq(fst, snd))
-            }
-
+            BinOp.MOD -> ctx.mkBvSignedModExpr(bv(x), bv(y))
+            BinOp.AND -> ctx.mkBvAndExpr(bv(x), bv(y))
+            BinOp.OR -> ctx.mkBvOrExpr(bv(x), bv(y))
+            BinOp.XOR -> ctx.mkBvXorExpr(bv(x), bv(y))
+            BinOp.SHL -> ctx.mkBvShiftLeftExpr(bv(x), bv(y))
+            BinOp.SHR -> ctx.mkBvArithShiftRightExpr(bv(x), bv(y))
+            BinOp.AND_NOT -> TODO()
+            BinOp.EQ -> ctx.mkEq(x, y)
             BinOp.LT -> {
-                outSort = ctx.boolSort
-                ctx.mkBvSignedLessExpr(fst, snd)
-            }
-
-            BinOp.LE -> {
-                outSort = ctx.boolSort
-                ctx.mkBvSignedLessOrEqualExpr(fst, snd)
+                val l = bv(x)
+                val r = bv(y)
+                if (signed) {
+                    ctx.mkBvSignedLessExpr(l, r)
+                } else {
+                    ctx.mkBvUnsignedLessExpr(l, r)
+                }
             }
 
             BinOp.GT -> {
-                outSort = ctx.boolSort
-                ctx.mkBvSignedGreaterExpr(fst, snd)
+                val l = bv(x)
+                val r = bv(y)
+                if (signed) {
+                    ctx.mkBvSignedGreaterExpr(l, r)
+                } else {
+                    ctx.mkBvUnsignedGreaterExpr(l, r)
+                }
+            }
+
+            BinOp.NEQ -> ctx.mkNot(ctx.mkEq(x, y))
+            BinOp.LE -> {
+                val l = bv(x)
+                val r = bv(y)
+                if (signed) {
+                    ctx.mkBvSignedLessOrEqualExpr(l, r)
+                } else {
+                    ctx.mkBvUnsignedLessOrEqualExpr(l, r)
+                }
             }
 
             BinOp.GE -> {
-                outSort = ctx.boolSort
-                ctx.mkBvSignedGreaterOrEqualExpr(fst, snd)
-            }
-
-            BinOp.ADD -> {
-                outSort = ctx.bv32Sort
-                ctx.mkBvAddExpr(fst, snd)
-            }
-
-            BinOp.SUB -> {
-                outSort = ctx.bv32Sort
-                ctx.mkBvSubExpr(fst, snd)
-            }
-
-            BinOp.MUL -> {
-                outSort = ctx.bv32Sort
-                ctx.mkBvMulExpr(fst, snd)
-            }
-
-            BinOp.DIV -> {
-                outSort = ctx.bv32Sort
-                ctx.mkBvSignedDivExpr(fst, snd)
-            }
-
-            BinOp.MOD -> {
-                outSort = ctx.bv32Sort
-                ctx.mkBvSignedModExpr(fst, snd)
+                val l = bv(x)
+                val r = bv(y)
+                if (signed) {
+                    ctx.mkBvSignedGreaterOrEqualExpr(l, r)
+                } else {
+                    ctx.mkBvUnsignedGreaterOrEqualExpr(l, r)
+                }
             }
 
             else -> null
         } ?: return
 
-        val lvalue = URegisterStackLValue(outSort, idx)
+        val lvalue = URegisterStackLValue(sort, idx)
         scope.doWithState {
-            memory.write(lvalue, expr.asExpr(outSort), ctx.trueExpr)
+            memory.write(lvalue, expr.asExpr(sort), ctx.trueExpr)
         }
     }
 
     private fun mkIf(buf: ByteBuffer) {
-        val idx = resolveIndex(VarKind.LOCAL, buf.int)
-        val lvalue = URegisterStackLValue(ctx.boolSort, idx)
-        val expression = scope.calcOnState { memory.read(lvalue) }.asExpr(ctx.boolSort)
+        val expression = resolveVar(buf) ?: return
         val pos = buf.long
         val neg = buf.long
         scope.forkWithBlackList(
-            expression,
+            expression.asExpr(ctx.boolSort),
             pos,
             neg,
             blockOnTrueState = { newInst(pos) },
@@ -127,7 +161,7 @@ class Api(
     }
 
     private fun mkReturn(buf: ByteBuffer) {
-        val value = resolveVar(ctx.bv32Sort, VarKind.valueOf(buf.get()), buf.int) ?: return
+        val value = resolveVar(buf) ?: return
         scope.doWithState {
             returnValue(value)
         }
@@ -135,21 +169,37 @@ class Api(
 
     private fun mkVariable(buf: ByteBuffer) {
         val idx = resolveIndex(VarKind.LOCAL, buf.int)
-        val lvalue = URegisterStackLValue(ctx.bv32Sort, idx)
-        val rvalue = resolveVar(ctx.bv32Sort, VarKind.valueOf(buf.get()), buf.int) ?: return
+        val rvalue = resolveVar(buf) ?: return
+        val lvalue = URegisterStackLValue(rvalue.sort, idx)
         scope.doWithState {
-            memory.write(lvalue, rvalue.asExpr(ctx.bv32Sort), ctx.trueExpr)
+            memory.write(lvalue, rvalue, ctx.trueExpr)
         }
     }
 
-    private fun <Sort : USort> resolveVar(sort: Sort, kind: VarKind, value: Int): UExpr<Sort>? {
-        val index = resolveIndex(kind, value)
+    private fun resolveVar(buf: ByteBuffer): UExpr<USort>? {
+        val kind = VarKind.valueOf(buf.get())
+        val type = Type.valueOf(buf.get())
+        val sort = toSort(type) ?: return null
         val expr = when (kind) {
-            VarKind.CONST -> ctx.mkBv(value)
-            VarKind.PARAMETER, VarKind.LOCAL -> scope.calcOnState { memory.read(URegisterStackLValue(sort, index)) }
-            VarKind.ILLEGAL -> null
+            VarKind.CONST -> readConst(buf, type)
+            VarKind.PARAMETER, VarKind.LOCAL -> scope.calcOnState {
+                memory.read(URegisterStackLValue(sort, resolveIndex(kind, buf.int)))
+            }
+
+            else -> null
         }
         return expr?.asExpr(sort)
+    }
+
+    private fun readConst(buf: ByteBuffer, type: Type): UExpr<out USort>? = when (type) {
+        Type.BOOL -> ctx.mkBool(buf.get() == 1.toByte())
+        Type.INT8, Type.UINT8 -> ctx.mkBv(buf.get(), ctx.bv8Sort)
+        Type.INT16, Type.UINT16 -> ctx.mkBv(buf.short, ctx.bv16Sort)
+        Type.INT32, Type.UINT32 -> ctx.mkBv(buf.int, ctx.bv32Sort)
+        Type.INT64, Type.UINT64 -> ctx.mkBv(buf.long, ctx.bv64Sort)
+        Type.FLOAT32 -> ctx.mkFp(buf.float, ctx.fp32Sort)
+        Type.FLOAT64 -> ctx.mkFp(buf.double, ctx.fp64Sort)
+        else -> null
     }
 
     private fun resolveIndex(kind: VarKind, value: Int): Int = when (kind) {
@@ -157,36 +207,77 @@ class Api(
         VarKind.PARAMETER -> value
         else -> -1
     }
+
+    private fun toSort(type: Type): USort? = when (type) {
+        Type.BOOL -> ctx.boolSort
+        Type.INT8, Type.UINT8 -> ctx.bv8Sort
+        Type.INT16, Type.UINT16 -> ctx.bv16Sort
+        Type.INT32, Type.UINT32 -> ctx.bv32Sort
+        Type.INT64, Type.UINT64 -> ctx.bv64Sort
+        Type.FLOAT32 -> ctx.fp32Sort
+        Type.FLOAT64 -> ctx.fp64Sort
+        else -> null
+    }
+
+    private fun bv(expr: UExpr<USort>): UExpr<UBvSort> {
+        return expr.asExpr(expr.sort as UBvSort)
+    }
 }
 
 private enum class Method(val value: Byte) {
     UNKNOWN(0),
-    MK_BIN_OP(1),
-    MK_IF(2),
-    MK_RETURN(3),
-    MK_VARIABLE(4);
+    MK_UN_OP(1),
+    MK_BIN_OP(2),
+    MK_IF(3),
+    MK_RETURN(4),
+    MK_VARIABLE(5);
 
     companion object {
-        fun valueOf(value: Byte) = Method.values().firstOrNull { it.value == value } ?: UNKNOWN
+        private val values = values()
+
+        fun valueOf(value: Byte) = values.firstOrNull { it.value == value } ?: UNKNOWN
+    }
+}
+
+private enum class UnOp(val value: Byte) {
+    ILLEGAL(0),
+    RECV(1),
+    NEG(2),
+    DEREF(3),
+    NOT(4),
+    INV(5);
+
+    companion object {
+        private val values = values()
+
+        fun valueOf(value: Byte) = values.firstOrNull { it.value == value } ?: ILLEGAL
     }
 }
 
 private enum class BinOp(val value: Byte) {
     ILLEGAL(0),
-    EQ(1),
-    NEQ(2),
-    LT(3),
-    LE(4),
-    GT(5),
-    GE(6),
-    ADD(7),
-    SUB(8),
-    MUL(9),
-    DIV(10),
-    MOD(11);
+    ADD(1),
+    SUB(2),
+    MUL(3),
+    DIV(4),
+    MOD(5),
+    AND(6),
+    OR(7),
+    XOR(8),
+    SHL(9),
+    SHR(10),
+    AND_NOT(11),
+    EQ(12),
+    LT(13),
+    GT(14),
+    NEQ(15),
+    LE(16),
+    GE(17);
 
     companion object {
-        fun valueOf(value: Byte) = BinOp.values().firstOrNull { it.value == value } ?: ILLEGAL
+        private val values = values()
+
+        fun valueOf(value: Byte) = values.firstOrNull { it.value == value } ?: ILLEGAL
     }
 }
 
@@ -197,6 +288,35 @@ private enum class VarKind(val value: Byte) {
     LOCAL(3);
 
     companion object {
-        fun valueOf(value: Byte) = VarKind.values().firstOrNull { it.value == value } ?: ILLEGAL
+        private val values = values()
+
+        fun valueOf(value: Byte) = values.firstOrNull { it.value == value } ?: ILLEGAL
+    }
+}
+
+private enum class Type(val value: Byte) {
+    UNKNOWN(0),
+    BOOL(1),
+    INT8(2),
+    UINT8(3),
+    INT16(4),
+    UINT16(5),
+    INT32(6),
+    UINT32(7),
+    INT64(8),
+    UINT64(9),
+    FLOAT32(10),
+    FLOAT64(11);
+
+    fun isSigned(): Boolean = when (this) {
+        BOOL, INT8, INT16, INT32, INT64, FLOAT32, FLOAT64 -> true
+        UINT8, UINT16, UINT32, UINT64 -> false
+        else -> false
+    }
+
+    companion object {
+        private val values = values()
+
+        fun valueOf(value: Byte) = values.firstOrNull { it.value == value } ?: UNKNOWN
     }
 }
