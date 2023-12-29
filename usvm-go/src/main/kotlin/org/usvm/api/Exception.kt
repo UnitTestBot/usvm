af8885d940b71722016df1e572878a57f227913a
Emil <buraindo24@gmail.com>
2023-12-29 03:18:48 +0300

    static calls seem to work

---
+++ usvm-go/src/main/kotlin/org/usvm/api/Exception.kt
@@ -0,0 +1,8 @@
+package org.usvm.api
+
+class UnknownSortException : Exception()
+class UnknownMethodException : Exception()
+class UnknownUnaryOperationException : Exception()
+class UnknownBinaryOperationException : Exception()
+class UnknownVarKindException : Exception()
+class UnknownTypeException : Exception()
