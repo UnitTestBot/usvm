35e75dc0d2ada7dd425ae407468ef2b6a930fb7c
tochilinak <tochilina.2002@yandex.ru>
2024-01-30 14:14:49 +0300

    Fixed visitor for recursive objects

--- usvm-python/usvm-python-object-model/src/main/kotlin/org/usvm/python/model/PyObjectModelVisitor.kt
+++ usvm-python/usvm-python-object-model/src/main/kotlin/org/usvm/python/model/PyObjectModelVisitor.kt
@@ -1,25 +1,53 @@
 package org.usvm.python.model
 
 open class PyObjectModelVisitor {
+    private val visited = mutableSetOf<PyObjectModel>()
     fun visit(obj: PyObjectModel) {
         obj.accept(this)
     }
-    open fun visit(obj: PyPrimitive) = run {}
-    open fun visit(obj: PyIdentifier) = run {}
-    open fun visit(obj: PyMockObject) = run {}
+    open fun visit(obj: PyPrimitive) {
+        visited.add(obj)
+    }
+    open fun visit(obj: PyIdentifier) {
+        visited.add(obj)
+    }
+    open fun visit(obj: PyMockObject) {
+        visited.add(obj)
+    }
     open fun visit(obj: PyCompositeObject) {
-        visit(obj.constructor)
-        obj.constructorArgs.forEach { visit(it) }
-        obj.listItems?.let { it.forEach { item -> visit(item) } }
-        obj.dictItems?.let {
-            it.forEach { (key, value) ->
-                visit(key)
-                visit(value)
+        visited.add(obj)
+        if (obj.constructor !in visited)
+            visit(obj.constructor)
+        obj.constructorArgs.forEach {
+            if (it !in visited)
+                visit(it)
+        }
+        obj.listItems?.let {
+            it.forEach { item ->
+                if (item !in visited)
+                    visit(item)
             }
         }
-        obj.fieldDict?.let { it.values.forEach { item -> visit(item) } }
+        obj.dictItems?.let {
+            it.forEach { (key, value) ->
+                if (key !in visited)
+                    visit(key)
+                if (value !in visited)
+                    visit(value)
+            }
+        }
+        obj.fieldDict?.let {
+            it.values.forEach { item ->
+                if (item !in visited)
+                    visit(item)
+            }
+        }
     }
     open fun visit(obj: PyTupleObject) {
-        obj.items.forEach { visit(it) }
+        visited.add(obj)
+        obj.items.forEach {
+            if (it !in visited)
+                visit(it)
+        }
     }
 }
