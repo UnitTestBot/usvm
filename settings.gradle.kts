0e829f983f225d36328c069b8ae1a9483bb35a1d
tochilinak <tochilina.2002@yandex.ru>
2024-01-31 18:16:32 +0300

    Made Python modules optional

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,6 @@ include("usvm-jvm")
 include("usvm-util")
 include("usvm-jvm-instrumentation")
 include("usvm-sample-language")
-include("usvm-python")
-include("usvm-python:cpythonadapter")
-findProject(":usvm-python:cpythonadapter")?.name = "cpythonadapter"
-include("usvm-python:usvm-python-annotations")
-findProject(":usvm-python:usvm-python-annotations")?.name = "usvm-python-annotations"
-include("usvm-python:usvm-python-main")
-findProject(":usvm-python:usvm-python-main")?.name = "usvm-python-main"
-include("usvm-python:usvm-python-runner")
-findProject(":usvm-python:usvm-python-runner")?.name = "usvm-python-runner"
 
 pluginManagement {
     resolutionStrategy {
@@ -24,5 +15,27 @@ pluginManagement {
         }
     }
 }
-include("usvm-python:usvm-python-object-model")
-findProject(":usvm-python:usvm-python-object-model")?.name = "usvm-python-object-model"
+
+// from GRADLE_USER_HOME/gradle.properties
+val githubUserFromHome: String? by settings
+val githubTokenFromHome: String? by settings  // with permission to read packages
+
+val githubUser: String? = githubUserFromHome ?: System.getenv("GITHUB_ACTOR")
+val githubToken: String? = githubTokenFromHome ?: System.getenv("GITHUB_TOKEN")
+val pythonActivated = githubUser != null && githubToken != null
+
+if (pythonActivated) {
+    include("usvm-python")
+    include("usvm-python:cpythonadapter")
+    findProject(":usvm-python:cpythonadapter")?.name = "cpythonadapter"
+    include("usvm-python:usvm-python-annotations")
+    findProject(":usvm-python:usvm-python-annotations")?.name = "usvm-python-annotations"
+    include("usvm-python:usvm-python-main")
+    findProject(":usvm-python:usvm-python-main")?.name = "usvm-python-main"
+    include("usvm-python:usvm-python-runner")
+    findProject(":usvm-python:usvm-python-runner")?.name = "usvm-python-runner"
+    include("usvm-python:usvm-python-object-model")
+    findProject(":usvm-python:usvm-python-object-model")?.name = "usvm-python-object-model"
+} else {
+    logger.warn("Warning: usvm-python modules are disabled")
+}
