3cd3c88b7cbbab6172b0c76631fa63c8fbe09f92
IlyaMuravjov <muravjovilya@gmail.com>
2024-02-01 10:44:53 +0300

    Add test for annotation default value instrumentation

--- usvm-jvm-instrumentation/src/samples/java/example/AnnotatedMethodClass.java
+++
@@ -1,8 +0,0 @@
-package example;
-
-public class AnnotatedMethodClass {
-    @MyAnnotation
-    public static int getSelfAnnotationCount() throws NoSuchMethodException {
-        return AnnotatedMethodClass.class.getMethod("getSelfAnnotationCount").getAnnotations().length;
-    }
-}
--- usvm-jvm-instrumentation/src/samples/java/example/MyAnnotation.java
+++ usvm-jvm-instrumentation/src/samples/java/example/MyAnnotation.java
@@ -5,4 +5,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotation {
+    String x() default "MyAnnotation default value";
 }
