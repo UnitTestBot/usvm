f85bc78f39b45dc89c6def9762f9fea433a3af28
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2024-02-29 12:15:46 +0300

    Updated usvm-python/buildDistWithoutPip.sh

--- usvm-python/buildDistWithoutPip.sh
+++ usvm-python/buildDistWithoutPip.sh
@@ -1,11 +1,11 @@
-../gradlew :usvm-python:cpythonadapter:CPythonClean &&\
-../gradlew :usvm-python:cpythonadapter:CPythonDistclean &&\
-../gradlew :usvm-python:cpythonadapter:clean &&\
-../gradlew :usvm-python:clean &&\
+../gradlew -PcpythonActivated=true :usvm-python:cpythonadapter:CPythonClean &&\
+../gradlew -PcpythonActivated=true :usvm-python:cpythonadapter:CPythonDistclean &&\
+../gradlew -PcpythonActivated=true :usvm-python:cpythonadapter:clean &&\
+../gradlew -PcpythonActivated=true :usvm-python:clean &&\
 echo "false" > cpythonadapter/include_pip_in_build &&\
-../gradlew :usvm-python:cpythonadapter:linkDebug &&\
-../gradlew :usvm-util:jar &&\
-../gradlew :usvm-core:jar &&\
-../gradlew :usvm-python:jar &&\
-../gradlew :usvm-python:distZip &&\
+../gradlew -PcpythonActivated=true :usvm-python:cpythonadapter:linkDebug &&\
+../gradlew -PcpythonActivated=true :usvm-util:jar &&\
+../gradlew -PcpythonActivated=true :usvm-core:jar &&\
+../gradlew -PcpythonActivated=true :usvm-python:jar &&\
+../gradlew -PcpythonActivated=true :usvm-python:distZip &&\
 echo "true" > cpythonadapter/include_pip_in_build
