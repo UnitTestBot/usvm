3bdb74531175bfcf041380de53c37c5830cf3348
Emil <buraindo24@gmail.com>
2024-03-10 02:13:01 +0300

    supported TypeAssert, fixed other casts

--- usvm-sample-language/src/test/kotlin/org/usvm/machine/LoopTest.kt
+++ usvm-sample-language/src/test/kotlin/org/usvm/machine/LoopTest.kt
@@ -7,15 +7,11 @@ import org.usvm.UMachineOptions
 import org.usvm.language.IntConst
 import org.usvm.programs.LoopProgram
 import kotlin.test.assertTrue
-import kotlin.time.Duration
 
 
 class LoopTest {
-    private val programDecl = LoopProgram
-    private val machine = SampleMachine(
-        programDecl.program,
-        UMachineOptions(listOf(PathSelectionStrategy.BFS), solverType = SolverType.Z3)
-    )
+    val programDecl = LoopProgram
+    val machine = SampleMachine(programDecl.program, UMachineOptions(listOf(PathSelectionStrategy.DFS), solverType = SolverType.YICES))
 
     @Test
     fun runLoopLowIdx() {
@@ -41,51 +37,5 @@ class LoopTest {
         assertTrue {
             results.any { it is SuccessfulExecutionResult && (it.outputModel.returnExpr as IntConst).const == 1 }
         }
-    }
-
-    @Test
-    fun runLoopSimple() {
-        val results = machine.analyze(programDecl.loopSimple)
-        println(results)
-    }
-
-    @Test
-    fun runLoopHard() {
-        val machine = SampleMachine(
-            programDecl.program,
-            UMachineOptions(listOf(PathSelectionStrategy.BFS), timeout = Duration.INFINITE, solverType = SolverType.Z3),
-        )
-        val results = machine.analyze(programDecl.loopHard)
-        println(results)
-    }
-
-    @Test
-    fun runLoopInfinite() {
-        val machine = SampleMachine(
-            programDecl.program,
-            UMachineOptions(
-                listOf(PathSelectionStrategy.DFS),
-                stopOnCoverage = -1,
-                timeout = Duration.INFINITE,
-                stepLimit = 1_000_000UL,
-            ),
-        )
-        val results = machine.analyze(programDecl.loopInfinite)
-        println(results)
-    }
-
-    @Test
-    fun runLoopCollatz() {
-        val machine = SampleMachine(
-            programDecl.program,
-            UMachineOptions(
-                listOf(PathSelectionStrategy.FORK_DEPTH),
-                stopOnCoverage = -1,
-                timeout = Duration.INFINITE,
-                stepLimit = 1_000_000UL,
-            ),
-        )
-        val results = machine.analyze(programDecl.loopCollatz)
-        println(results)
     }
 }
--- usvm-sample-language/src/test/kotlin/org/usvm/programs/LoopProgram.kt
+++ usvm-sample-language/src/test/kotlin/org/usvm/programs/LoopProgram.kt
@@ -2,10 +2,17 @@ package org.usvm.programs
 
 import org.usvm.language.ArrayType
 import org.usvm.language.IntType
-import org.usvm.language.builders.*
+import org.usvm.language.builders.ProgramDecl
+import org.usvm.language.builders.eq
+import org.usvm.language.builders.expr
+import org.usvm.language.builders.get
+import org.usvm.language.builders.invoke
+import org.usvm.language.builders.lt
+import org.usvm.language.builders.method
+import org.usvm.language.builders.plus
 
 object LoopProgram : ProgramDecl() {
-    private const val CONST = 1_000
+    const val CONST = 1_000
 
     val loopHighIdx by method(IntType, IntType, returnType = IntType) { _, a ->
         val arr by ArrayType(IntType)(size = CONST.expr)
@@ -37,61 +44,5 @@ object LoopProgram : ProgramDecl() {
             ret(1.expr)
         }
         ret(0.expr)
-    }
-
-    val loopSimple by method(IntType, returnType = IntType) {
-        var idx by 0.expr
-        loop(idx lt 10.expr) {
-            idx += 1.expr
-        }
-        ret(0.expr)
-    }
-
-    val loopHard by method(IntType, IntType, returnType = IntType) { i, j ->
-        var sum by 0.expr
-        var k by 0.expr
-        loop(k lt 100.expr) {
-            var l by 0.expr
-            loop(l lt 100.expr) {
-                branch((k + l) lt (i + j)) {
-                    sum += (k + l)
-                }
-                l += 1.expr
-            }
-            k += 1.expr
-        }
-        ret(sum)
-    }
-
-    val loopInfinite by method(IntType, returnType = IntType) { i ->
-        loop(true.expr) {
-            var j by i + 3.expr
-            val k by j + i
-            val l by k + j + 4.expr
-            j = l + 2.expr
-        }
-    }
-
-    val loopCollatz by method(IntType, returnType = IntType) { i ->
-        branch((i le 0.expr) or (i ge 100.expr)) {
-            ret(0.expr)
-        }
-
-        var j by i
-        var loopCount by 0.expr
-
-        loop((j eq 1.expr).not()) {
-            val oldJ by j
-            j = oldJ * 3.expr + 1.expr
-            branch((oldJ % 2.expr) eq 0.expr) {
-                j = oldJ / 2.expr
-            }
-            loopCount += 1.expr
-        }
-
-        branch(loopCount eq 17.expr) {
-            ret(1.expr)
-        }
-        ret(2.expr)
     }
 }
