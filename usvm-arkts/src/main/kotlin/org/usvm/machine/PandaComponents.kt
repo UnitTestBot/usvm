c3ed169f75d866861ea1234e4c46e146e0ef62ca
Sergey Loktev <loktevsergay.2003@gmail.com>
2024-03-25 14:56:07 +0300

    PandaComponents Implementation

--- usvm-arkts/src/main/kotlin/org/usvm/machine/PandaComponents.kt
+++ usvm-arkts/src/main/kotlin/org/usvm/machine/PandaComponents.kt
@@ -1,30 +1,65 @@
 package org.usvm.machine
 
+import io.ksmt.expr.KBitVec64Value
+import io.ksmt.solver.yices.KYicesSolver
+import io.ksmt.solver.z3.KZ3Solver
 import org.jacodb.panda.dynamic.api.PandaType
-import org.usvm.UBv32Sort
-import org.usvm.UComponents
-import org.usvm.UContext
-import org.usvm.UMachineOptions
-import org.usvm.USizeExprProvider
+import org.usvm.*
 import org.usvm.solver.USolverBase
+import org.usvm.solver.UTypeSolver
 
-
+/*
+    Numbers in JavaScript are signed 64 bit doubles.
+ */
 class PandaComponents(
     private val typeSystem: PandaTypeSystem,
     private val options: UMachineOptions
-) : UComponents<PandaType, UBv32Sort> { // TODO Usizesort????????? {
-    override val useSolverForForks: Boolean
-        get() = TODO("Not yet implemented")
+) : UComponents<PandaType, PandaFp64Sort> {
+    private val closeableResources = mutableListOf<AutoCloseable>()
+    override val useSolverForForks: Boolean get() = options.useSolverForForks
 
-    override fun <Context : UContext<UBv32Sort>> mkSolver(ctx: Context): USolverBase<PandaType> {
-        TODO("Not yet implemented")
+    override fun <Context : UContext<PandaFp64Sort>> mkSolver(ctx: Context): USolverBase<PandaType> {
+        val (translator, decoder) = buildTranslatorAndLazyDecoder(ctx)
+        val solver = when (options.solverType) {
+            SolverType.YICES -> KYicesSolver(ctx)
+            SolverType.Z3 -> KZ3Solver(ctx)
+        }
+        val typeSolver = UTypeSolver(typeSystem)
+
+        return USolverBase(ctx, solver, typeSolver, translator, decoder, options.solverTimeout)
     }
 
-    override fun mkTypeSystem(ctx: UContext<UBv32Sort>): PandaTypeSystem {
-        TODO("Not yet implemented")
+    fun close() {
+        closeableResources.forEach(AutoCloseable::close)
     }
 
-    override fun <Context : UContext<UBv32Sort>> mkSizeExprProvider(ctx: Context): USizeExprProvider<UBv32Sort> {
-        TODO("Not yet implemented")
+    override fun mkTypeSystem(ctx: UContext<PandaFp64Sort>): PandaTypeSystem {
+        return typeSystem
     }
+
+    override fun <Context : UContext<PandaFp64Sort>> mkSizeExprProvider(ctx: Context): USizeExprProvider<PandaFp64Sort> {
+        return PandaFpSortSizeExprProvider(ctx as PandaContext)
+    }
+}
+
+class PandaFpSortSizeExprProvider(
+    override val ctx: PandaContext
+) : USizeExprProvider<PandaFp64Sort> {
+    override val sizeSort: PandaFp64Sort = ctx.fp64Sort
+
+    override fun mkSizeExpr(size: Int): UExpr<PandaFp64Sort> = ctx.mkFp(size.toDouble(), sizeSort)
+    override fun getIntValue(expr: UExpr<PandaFp64Sort>): Int? = (expr as? KBitVec64Value)?.numberValue?.toInt()
+
+    override fun mkSizeSubExpr(lhs: UExpr<PandaFp64Sort>, rhs: UExpr<PandaFp64Sort>): UExpr<PandaFp64Sort> =
+        ctx.mkFpSubExpr(ctx.fpRoundingModeSortDefaultValue(), lhs, rhs)
+    override fun mkSizeAddExpr(lhs: UExpr<PandaFp64Sort>, rhs: UExpr<PandaFp64Sort>): UExpr<PandaFp64Sort> =
+        ctx.mkFpAddExpr(ctx.fpRoundingModeSortDefaultValue(), lhs, rhs)
+    override fun mkSizeGtExpr(lhs: UExpr<PandaFp64Sort>, rhs: UExpr<PandaFp64Sort>): UBoolExpr =
+        ctx.mkFpGreaterExpr(lhs, rhs)
+    override fun mkSizeGeExpr(lhs: UExpr<PandaFp64Sort>, rhs: UExpr<PandaFp64Sort>): UBoolExpr =
+        ctx.mkFpGreaterOrEqualExpr(lhs, rhs)
+    override fun mkSizeLtExpr(lhs: UExpr<PandaFp64Sort>, rhs: UExpr<PandaFp64Sort>): UBoolExpr =
+        ctx.mkFpLessExpr(lhs, rhs)
+    override fun mkSizeLeExpr(lhs: UExpr<PandaFp64Sort>, rhs: UExpr<PandaFp64Sort>): UBoolExpr =
+        ctx.mkFpLessOrEqualExpr(lhs, rhs)
 }
--- usvm-arkts/src/main/kotlin/org/usvm/machine/PandaContext.kt
+++ usvm-arkts/src/main/kotlin/org/usvm/machine/PandaContext.kt
@@ -1,8 +1,6 @@
 package org.usvm.machine
 
-import org.usvm.UBv32Sort
 import org.usvm.UContext
 
-// TODO size sort????
-class PandaContext(components: PandaComponents) : UContext<UBv32Sort>(components) {
+class PandaContext(components: PandaComponents) : UContext<PandaFp64Sort>(components) {
 }
