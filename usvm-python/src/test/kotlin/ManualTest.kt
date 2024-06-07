531b839340f4ad08f92aa85d8a4d90f35a9a5a47
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2024-06-07 16:54:48 +0300

    Finished refactoring manualTestKt

--- usvm-python/src/test/kotlin/ManualTest.kt
+++ usvm-python/src/test/kotlin/ManualTest.kt
@@ -1,13 +1,9 @@
-import org.usvm.UMachineOptions
-import org.usvm.language.PyProgram
-import org.usvm.language.PyUnpinnedCallable
-import org.usvm.machine.interpreters.concrete.IllegalOperationException
-import org.usvm.machine.types.PythonTypeSystem
-import org.usvm.runner.CustomPythonTestRunner
 import org.usvm.runner.manual.analyzers.OrdinaryAnalyzer
-import org.usvm.runner.manual.program.LocalProgramProvider
-import kotlin.time.Duration.Companion.seconds
+import org.usvm.runner.manual.program.sampleFunction
 
+/**
+ * This should be run with task `manualTestDebug` or `manualTestDebugNoLogs`
+ * */
 fun main() {
     /**
      * See:
@@ -15,48 +11,14 @@ fun main() {
      *  - [org.usvm.runner.manual.program.sampleFunction]
      *  - [org.usvm.runner.manual.program.LocalProgramProvider]
      * */
-    val program = LocalProgramProvider(
-        "/home/tochilinak/Documents/projects/utbot/Python/dynamic_programming",
-    )
+    val program = sampleFunction
 
     /**
-     * TODO
+     * See:
+     *  - [org.usvm.runner.manual.analyzers.OrdinaryAnalyzer]
+     *  - [org.usvm.runner.manual.analyzers.ConcolicAndConcreteChecker]
      * */
     val analyzer = OrdinaryAnalyzer
 
     analyzer.run(program)
 }
-
-
-private fun checkConcolicAndConcrete(runConfig: RunConfig) {
-    val (program, typeSystem, functions) = runConfig
-    val runner = CustomPythonTestRunner(
-        program,
-        typeSystem,
-        UMachineOptions(stepLimit = 60U, timeout = 60.seconds),
-        allowPathDiversions = true
-    )
-    runner.timeoutPerRunMs = 10_000
-    functions.forEach { function ->
-        println("Running ${function.tag}...")
-        try {
-            val comparator = runner.standardConcolicAndConcreteChecks
-            when (val argsNum = function.numberOfArguments) {
-                0 -> runner.check0NoPredicates(function, comparator)
-                1 -> runner.check1NoPredicates(function, comparator)
-                2 -> runner.check2NoPredicates(function, comparator)
-                3 -> runner.check3NoPredicates(function, comparator)
-                4 -> runner.check4NoPredicates(function, comparator)
-                else -> println("${function.tag} ignored because it has $argsNum arguments")
-            }
-        } catch (e: IllegalOperationException) {
-            println("Illegal operation while analyzing: ${e.operation}\n")
-        }
-    }
-}
-
-private data class RunConfig(
-    val program: PyProgram,
-    val typeSystem: PythonTypeSystem,
-    val functions: List<PyUnpinnedCallable>,
-)
---
+++ usvm-python/src/test/kotlin/org/usvm/runner/manual/analyzers/ConcolicAndConcreteChecker.kt
@@ -0,0 +1,35 @@
+package org.usvm.runner.manual.analyzers
+
+import org.usvm.UMachineOptions
+import org.usvm.machine.interpreters.concrete.IllegalOperationException
+import org.usvm.runner.CustomPythonTestRunner
+import org.usvm.runner.manual.program.ProgramProvider
+import kotlin.time.Duration.Companion.seconds
+
+class ConcolicAndConcreteChecker: ProgramAnalyzer {
+    override fun run(provider: ProgramProvider) {
+        val runner = CustomPythonTestRunner(
+            provider.program,
+            provider.typeSystem,
+            UMachineOptions(stepLimit = 60U, timeout = 60.seconds),
+            allowPathDiversions = true
+        )
+        runner.timeoutPerRunMs = 10_000
+        provider.functions.forEach { function ->
+            println("Running ${function.tag}...")
+            try {
+                val comparator = runner.standardConcolicAndConcreteChecks
+                when (val argsNum = function.numberOfArguments) {
+                    0 -> runner.check0NoPredicates(function, comparator)
+                    1 -> runner.check1NoPredicates(function, comparator)
+                    2 -> runner.check2NoPredicates(function, comparator)
+                    3 -> runner.check3NoPredicates(function, comparator)
+                    4 -> runner.check4NoPredicates(function, comparator)
+                    else -> println("${function.tag} ignored because it has $argsNum arguments")
+                }
+            } catch (e: IllegalOperationException) {
+                println("Illegal operation while analyzing: ${e.operation}\n")
+            }
+        }
+    }
+}
