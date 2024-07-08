feb249b851a9e2e9490015c15cc53ab41143ef70
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2024-07-08 16:50:06 +0300

    fixes in usvm-python-runner

--- usvm-python/usvm-python-runner/src/test/kotlin/org/usvm/runner/ManualTest.kt
+++ usvm-python/usvm-python-runner/src/test/kotlin/org/usvm/runner/ManualTest.kt
@@ -1,15 +1,25 @@
 package org.usvm.runner
 
+import mu.KLogging
 import org.usvm.python.ps.PyPathSelectorType
 import java.io.File
 
+/**
+ * Should be run with task `manualTestOfRunner` after building jar of main usvm-python part.
+ * */
 fun main() {
+    run()
+}
+
+private val logger = object : KLogging() {}.logger
+
+fun run(useDebugRunner: Boolean = false) {
     val start = System.currentTimeMillis()
     val basePath = System.getProperty("project.root")
-    val layout = TestingLayout(basePath) // StandardLayout(File(basePath, "build/distributions/usvm-python"))
+    val layout = TestingLayout(basePath)
     val mypyDir = File(basePath, "build/samples_build")
     val root = File(basePath, "src/test/resources/samples")
-    val venvConfig = null // extractVenvConfig("/home/tochilinak/sample_venv/bin/python")
+    val venvConfig = null
     val config = USVMPythonConfig(
         layout,
         "java",
@@ -26,15 +36,19 @@ fun main() {
         20_000,
         3_000
     )
-    /*val debugRunner = DebugRunner(config)
-    debugRunner.use {
-        it.runProcessAndPrintInfo(runConfig)
-    }*/
-    val receiver = PrintingResultReceiver()
-    val runner = PythonSymbolicAnalysisRunnerImpl(config)
-    runner.use {
-        it.analyze(runConfig, receiver) { System.currentTimeMillis() - start >= 20_000 }
+    if (!useDebugRunner) {
+        /**/
+        val receiver = PrintingResultReceiver()
+        val runner = PythonSymbolicAnalysisRunnerImpl(config)
+        runner.use {
+            it.analyze(runConfig, receiver) { System.currentTimeMillis() - start >= 20_000 }
+        }
+        logger.info("Number of executions: ${receiver.cnt}")
+    } else {
+        val debugRunner = DebugRunner(config)
+        debugRunner.use {
+            it.runProcessAndPrintInfo(runConfig)
+        }
     }
-    println("Time: ${System.currentTimeMillis() - start}")
-    println("Number of executions: ${receiver.cnt}")
+    logger.info("Time: ${System.currentTimeMillis() - start}")
 }
