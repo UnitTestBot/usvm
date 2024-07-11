dcb5203e2accbec3ae9a2b3fd6a2dff4cb8999d6
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2024-07-11 14:24:29 +0300

    removed duplicated lines

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,8 @@ include("usvm-python:usvm-python-main")
 findProject(":usvm-python:usvm-python-main")?.name = "usvm-python-main"
 include("usvm-python:usvm-python-runner")
 findProject(":usvm-python:usvm-python-runner")?.name = "usvm-python-runner"
+include("usvm-python:usvm-python-commons")
+findProject(":usvm-python:usvm-python-commons")?.name = "usvm-python-commons"
 
 pluginManagement {
     resolutionStrategy {
@@ -27,15 +29,3 @@ pluginManagement {
         }
     }
 }
-
-include("usvm-python")
-include("usvm-python:cpythonadapter")
-findProject(":usvm-python:cpythonadapter")?.name = "cpythonadapter"
-include("usvm-python:usvm-python-annotations")
-findProject(":usvm-python:usvm-python-annotations")?.name = "usvm-python-annotations"
-include("usvm-python:usvm-python-main")
-findProject(":usvm-python:usvm-python-main")?.name = "usvm-python-main"
-include("usvm-python:usvm-python-runner")
-findProject(":usvm-python:usvm-python-runner")?.name = "usvm-python-runner"
-include("usvm-python:usvm-python-commons")
-findProject(":usvm-python:usvm-python-commons")?.name = "usvm-python-commons"
