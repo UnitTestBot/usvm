6680dc44b28db5eaa3ad695a5769b0de7ad94a93
Alexey Menshutin <alex.menshutin99@gmail.com>
2024-07-25 15:17:05 +0300

    TS test framework improvements (#199)

--- usvm-ts/src/main/kotlin/org/usvm/TSTest.kt
+++ usvm-ts/src/main/kotlin/org/usvm/TSTest.kt
@@ -8,4 +8,33 @@ class TSTest(
     val trace: List<EtsStmt>? = null,
 )
 
-class TSMethodCoverage
+open class TSMethodCoverage
+
+object NoCoverage : TSMethodCoverage()
+
+sealed interface TSObject {
+    sealed interface TSNumber : TSObject {
+        data class Integer(val value: Int) : TSNumber
+
+        data class Double(val value: kotlin.Double) : TSNumber
+
+        val number: kotlin.Double
+            get() = when (this) {
+                is Integer -> value.toDouble()
+                is Double -> value
+            }
+    }
+
+    data class String(val value: kotlin.String) : TSObject
+
+    data class Boolean(val value: kotlin.Boolean) : TSObject
+
+
+    data class Class(val name: String, val properties: Map<String, TSObject>) : TSObject
+
+    data object AnyObject : TSObject
+
+    data object UndefinedObject : TSObject
+
+    data class Array(val values: List<TSObject>) : TSObject
+}
