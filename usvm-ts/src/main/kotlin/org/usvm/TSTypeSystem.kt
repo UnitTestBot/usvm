b15d48248f3d361be8215e02af132b5641f73f6f
Sergey Loktev <loktevsergay.2003@gmail.com>
2024-07-29 13:22:26 +0300

    Enhance TSTypeSystem + jacodb version update

--- usvm-ts/src/main/kotlin/org/usvm/TSTypeSystem.kt
+++ usvm-ts/src/main/kotlin/org/usvm/TSTypeSystem.kt
@@ -1,10 +1,12 @@
 package org.usvm
 
-import org.jacodb.ets.base.EtsPrimitiveType
-import org.jacodb.ets.base.EtsType
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import org.jacodb.ets.base.*
+import org.jacodb.ets.model.EtsClassSignature
 import org.jacodb.ets.model.EtsFile
-import org.usvm.types.UTypeStream
-import org.usvm.types.UTypeSystem
+import org.usvm.types.*
+import org.usvm.types.TypesResult.Companion.toTypesResult
 import kotlin.time.Duration
 
 class TSTypeSystem(
@@ -13,28 +15,127 @@ class TSTypeSystem(
 ) : UTypeSystem<EtsType> {
 
     override fun isSupertype(supertype: EtsType, type: EtsType): Boolean {
-       if (supertype == type) return true
-
-        if (type is EtsPrimitiveType) return false
+        return when {
+            supertype == type -> return true
+            type is EtsPrimitiveType -> return false
+            type is EtsClassType -> supertype.checkSubtype(type.classSignature)
+            else -> false
+        }
     }
 
     override fun hasCommonSubtype(type: EtsType, types: Collection<EtsType>): Boolean {
-        TODO("Not yet implemented")
+        when (type) {
+            is EtsPrimitiveType -> return types.isEmpty()
+            is EtsClassType -> return types.all { isSupertype(it, type) }
+            is EtsArrayType -> {
+                val elementTypes = types.map {
+                    when {
+                        it is EtsArrayType -> it.elementType
+                        else -> return false
+                    }
+                }
+                return hasCommonSubtype(type.elementType, elementTypes)
+            }
+            else -> TODO()
+        }
     }
 
     override fun isFinal(type: EtsType): Boolean {
-        TODO("Not yet implemented")
+        // No final modifier in jacodb IR
+        return false
     }
 
     override fun isInstantiable(type: EtsType): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
-    override fun findSubtypes(type: EtsType): Sequence<EtsType> {
-        TODO("Not yet implemented")
+    override fun findSubtypes(type: EtsType): Sequence<EtsType> = when (type) {
+        is EtsPrimitiveType -> emptySequence()
+        is EtsArrayType -> findSubtypes(type.elementType).map { EtsArrayType(it, type.dimensions) }
+        is EtsClassType -> TODO()
+        else -> TODO()
     }
 
     override fun topTypeStream(): UTypeStream<EtsType> {
-        TODO("Not yet implemented")
+        return TSTopTypeStream(this)
     }
+
+    private fun EtsType.checkSubtype(typeClassSignature: EtsClassSignature?): Boolean {
+        if (typeClassSignature == null) return false
+
+        return this.typeName == typeClassSignature.name ||
+                this.checkSubtype(project.getClassBySignature(typeClassSignature)?.superClass)
+    }
+}
+
+class TSTopTypeStream(
+    private val typeSystem: TSTypeSystem,
+    val primitiveTypes: PersistentList<EtsPrimitiveType> = persistentListOf(
+        EtsNumberType,
+        EtsBooleanType,
+        EtsStringType
+    ),
+) : UTypeStream<EtsType> {
+    override fun filterBySupertype(type: EtsType): UTypeStream<EtsType> {
+        if (type is EtsPrimitiveType) {
+            if (type !in primitiveTypes) {
+                return emptyTypeStream()
+            }
+
+            return USingleTypeStream(typeSystem, type)
+        }
+
+        return emptyTypeStream()
+    }
+
+    override fun filterBySubtype(type: EtsType): UTypeStream<EtsType> {
+        if (type is EtsPrimitiveType) {
+            if (type !in primitiveTypes) {
+                return emptyTypeStream()
+            }
+
+            return USingleTypeStream(typeSystem, type)
+        }
+
+        return emptyTypeStream()
+    }
+
+    override fun filterByNotSupertype(type: EtsType): UTypeStream<EtsType> {
+        if (type is EtsPrimitiveType && type in primitiveTypes) {
+            val updatedPrimitiveTypes = primitiveTypes.remove(type)
+
+            if (updatedPrimitiveTypes.isEmpty()) {
+                return emptyTypeStream()
+            }
+
+            return TSTopTypeStream(typeSystem, updatedPrimitiveTypes)
+        }
+
+        return TSTopTypeStream(typeSystem, primitiveTypes)
+    }
+
+    override fun filterByNotSubtype(type: EtsType): UTypeStream<EtsType> {
+        if (type is EtsPrimitiveType && type in primitiveTypes) {
+            val updatedPrimitiveTypes = primitiveTypes.remove(type)
+
+            if (updatedPrimitiveTypes.isEmpty()) {
+                return emptyTypeStream()
+            }
+
+            return TSTopTypeStream(typeSystem, updatedPrimitiveTypes)
+        }
+
+        return TSTopTypeStream(typeSystem, primitiveTypes)
+    }
+
+    override fun take(n: Int): TypesResult<EtsType> {
+        return primitiveTypes.take(n).toTypesResult(wasTimeoutExpired = false)
+    }
+
+    override val isEmpty: Boolean
+        get() = primitiveTypes.isEmpty()
+
+    override val commonSuperType: EtsType
+        get() = EtsAnyType
+
 }
