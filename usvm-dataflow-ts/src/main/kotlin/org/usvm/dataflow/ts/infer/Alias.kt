332f996302692f3526c349b49e885911cabbd3e3
Konstantin Chukharev <lipen00@gmail.com>
2024-09-13 18:01:05 +0300

    Improve alias analysis

--- usvm-dataflow-ts/src/main/kotlin/org/usvm/dataflow/ts/infer/Alias.kt
+++ usvm-dataflow-ts/src/main/kotlin/org/usvm/dataflow/ts/infer/Alias.kt
@@ -16,16 +16,35 @@
 
 package org.usvm.dataflow.ts.infer
 
+import org.jacodb.ets.base.EtsAssignStmt
+import org.jacodb.ets.base.EtsCallExpr
+import org.jacodb.ets.base.EtsCastExpr
+import org.jacodb.ets.base.EtsConstant
+import org.jacodb.ets.base.EtsLocal
+import org.jacodb.ets.base.EtsNewArrayExpr
+import org.jacodb.ets.base.EtsNewExpr
+import org.jacodb.ets.base.EtsParameterRef
+import org.jacodb.ets.base.EtsRef
 import org.jacodb.ets.base.EtsStmt
 import org.jacodb.ets.model.EtsMethod
-import org.usvm.dataflow.ifds.Accessor
+import org.usvm.dataflow.ifds.ElementAccessor
 import org.usvm.dataflow.ifds.FieldAccessor
 
 sealed interface Allocation {
     class New : Allocation
     data class Arg(val index: Int) : Allocation
     class CallResult : Allocation
+    class Imm : Allocation
 }
+
+// class Allocation2(val index: Int?) {
+//     override fun equals(other: Any?): Boolean {
+//         if (other === this) return true
+//         if (other !is Allocation2) return false
+//         if (index != null) return index == other.index
+//         return false
+//     }
+// }
 
 class AliasingInfo(
     // B: Base -> Object
@@ -33,15 +52,18 @@ class AliasingInfo(
     // F: Object x Field -> Object
     val F: Map<Allocation, Map<String, Allocation>>,
 ) {
+    // A: Base x Field* -> Object
     fun find(path: AccessPath): Allocation? {
         val b = B[path.base] ?: return null
         if (path.accesses.isEmpty()) return b
-        val f = F[b] ?: return null
-        val a = path.accesses.single()
-        check(a is FieldAccessor) {
-            "Sorry, arrays are not supported, yet"
+        return when (val a = path.accesses.single()) {
+            is FieldAccessor -> {
+                val f = F[b] ?: return null
+                f[a.name]
+            }
+
+            is ElementAccessor -> null
         }
-        return f[a.name]
     }
 
     fun merge(other: AliasingInfo): AliasingInfo {
@@ -60,8 +82,7 @@ class AliasingInfo(
         return AliasingInfo(newB, newF)
     }
 
-    fun getAliases(obj: Allocation): Set<AccessPath> {
-        // TODO: traverse graph backward
+    private fun getAliases(obj: Allocation): Set<AccessPath> {
         val paths = mutableSetOf<AccessPath>()
 
         val invF = mutableMapOf<Allocation, MutableMap<String, MutableList<Allocation>>>()
@@ -81,16 +102,19 @@ class AliasingInfo(
 
         val queue = ArrayDeque<Pair<Allocation, List<FieldAccessor>>>(listOf(obj to emptyList()))
         while (queue.isNotEmpty()) {
-            val (cur, accessors) = queue.removeFirst()
+            val (cur, path) = queue.removeFirst()
+            // TODO: eliminate loops as in computeAliases via DFS with PATH/STACK
+            // TODO: think about loop-edges
+            if (path.size > 10) continue
             if (cur in invB) {
                 for (base in invB[cur]!!) {
-                    paths.add(AccessPath(base, accessors.reversed()))
+                    paths.add(AccessPath(base, path.reversed()))
                 }
             }
             if (cur in invF) {
                 for ((field, objs) in invF[cur]!!) {
                     for (alloc in objs) {
-                        queue.add(alloc to accessors + FieldAccessor(field))
+                        queue.add(alloc to path + FieldAccessor(field))
                     }
                 }
             }
@@ -108,20 +132,156 @@ class AliasingInfo(
 fun computeAliases(method: EtsMethod): Map<EtsStmt, AliasingInfo> {
     val aliases = mutableMapOf<EtsStmt, AliasingInfo>()
 
+    val root = method.cfg.stmts[0]
+    val queue = ArrayDeque(listOf(root))
+    val preds = mutableMapOf<EtsStmt, MutableList<EtsStmt>>()
+    val order = mutableListOf<EtsStmt>()
+    val visited = mutableSetOf<EtsStmt>()
+
+    while (queue.isNotEmpty()) {
+        val cur = queue.first()
+
+        if (visited.add(cur)) {
+            for (next in method.cfg.successors(cur)) {
+                if (next !in visited) {
+                    queue.addFirst(next)
+                }
+            }
+        } else {
+            order.add(cur)
+            queue.removeFirst()
+        }
+    }
+
     fun compute(stmt: EtsStmt): AliasingInfo {
         if (stmt in aliases) return aliases[stmt]!!
 
-        val preds = method.cfg.predecessors(stmt).map { compute(it) }
-        val merged = preds.reduceOrNull { a, b -> a.merge(b) } ?: AliasingInfo(emptyMap(), emptyMap())
+        val merged = preds[stmt].orEmpty()
+            .map { aliases[it]!! }
+            .reduceOrNull { a, b -> a.merge(b) }
+            ?: AliasingInfo(emptyMap(), emptyMap())
 
-        val newInfo = TODO("handle assignment")
+        val newB = merged.B.toMap(HashMap())
+        val newF = merged.F.mapValuesTo(HashMap()) { it.value.toMutableMap() }
 
+        if (stmt is EtsAssignStmt) {
+            val lhv = stmt.lhv
+            val rhv = stmt.rhv
+
+            if (rhv is EtsLocal || rhv is EtsRef || (rhv is EtsCastExpr && rhv.arg is EtsRef)) {
+                val lhs = lhv.toPath()
+                val rhs = rhv.toPath()
+
+                if (rhv is EtsParameterRef) {
+                    check(lhs.accesses.isEmpty())
+                    newB[lhs.base] = Allocation.Arg(rhv.index)
+                } else {
+                    if (lhs.accesses.isEmpty() && rhs.accesses.isEmpty()) {
+                        // x := y
+                        newB[lhs.base] = newB.computeIfAbsent(rhs.base) { Allocation.Imm() }
+                    } else if (lhs.accesses.isEmpty()) {
+                        // x := y.f  OR  x := y[i]
+                        when (val a = rhs.accesses.single()) {
+                            is FieldAccessor -> {
+                                // x := y.f
+                                val b: Allocation = newB.computeIfAbsent(rhs.base) { Allocation.Imm() }
+                                val f: MutableMap<String, Allocation> = newF.computeIfAbsent(b) { hashMapOf() }
+                                newB[lhs.base] = f.computeIfAbsent(a.name) { Allocation.Imm() }
+                            }
+
+                            is ElementAccessor -> {
+                                // x := y[i]
+                                newB.remove(lhs.base)
+                            }
+                        }
+                    } else if (rhs.accesses.isEmpty()) {
+                        // x.f := y  OR  x[i] := y
+                        when (val a = lhs.accesses.single()) {
+                            is FieldAccessor -> {
+                                // x.f := y
+                                val b: Allocation = newB.computeIfAbsent(rhs.base) { Allocation.Imm() }
+                                val f: MutableMap<String, Allocation> = newF.computeIfAbsent(b) { hashMapOf() }
+                                f[a.name] = newB.computeIfAbsent(rhs.base) { Allocation.Imm() }
+                            }
+
+                            is ElementAccessor -> {
+                                // x[i] := y
+                                // do nothing
+                            }
+                        }
+                    } else {
+                        error("Incorrect 3AC: $stmt")
+                    }
+                }
+            }
+
+            if (rhv is EtsConstant || (rhv is EtsCastExpr && rhv.arg is EtsConstant)) {
+                val lhs = lhv.toPath()
+                if (lhs.accesses.isEmpty()) {
+                    // x := const
+                    newB.remove(lhs.base)
+                } else {
+                    when (val a = lhs.accesses.single()) {
+                        is FieldAccessor -> {
+                            // x.f := const
+                            val b = newB.computeIfAbsent(lhs.base) { Allocation.Imm() }
+                            val f = newF.computeIfAbsent(b) { hashMapOf() }
+                            f.remove(a.name)
+                        }
+
+                        is ElementAccessor -> {
+                            // x[i] := const
+                            // do nothing
+                        }
+                    }
+                }
+            }
+
+            if (rhv is EtsNewExpr || rhv is EtsNewArrayExpr) {
+                val lhs = lhv.toPath()
+                if (lhs.accesses.isEmpty()) {
+                    // x := new()
+                    newB.computeIfAbsent(lhs.base) { Allocation.New() }
+                } else {
+                    when (val a = lhs.accesses.single()) {
+                        is FieldAccessor -> {
+                            // x.f := new()
+                            val b = newB.computeIfAbsent(lhs.base) { Allocation.Imm() }
+                            val f = newF.computeIfAbsent(b) { hashMapOf() }
+                            f[a.name] = Allocation.New()
+                        }
+
+                        is ElementAccessor -> {
+                            // x[i] := new()
+                            // do nothing
+                        }
+                    }
+                }
+            }
+
+            if (rhv is EtsCastExpr) {
+                check(rhv.arg !is EtsCallExpr)
+                check(rhv.arg is EtsLocal || rhv.arg is EtsRef || rhv.arg is EtsConstant)
+            }
+
+            if (rhv is EtsCallExpr) {
+                val lhs = lhv.toPath()
+                check(lhs.accesses.isEmpty())
+                newB[lhs.base] = Allocation.CallResult()
+            }
+        }
+
+        val newInfo = AliasingInfo(newB, newF)
         aliases[stmt] = newInfo
         return newInfo
     }
 
-    for (stmt in method.cfg.stmts) {
+    for (stmt in order) {
         compute(stmt)
+    }
+
+    for (stmt in method.cfg.stmts) {
+        check(stmt in aliases)
     }
 
     return aliases
