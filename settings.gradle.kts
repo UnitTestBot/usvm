ce54887fe52c08586b9ee0bdbd45197050d08a87
Sergey Loktev <loktevsergay.2003@gmail.com>
2024-10-02 17:33:37 +0300

    Revert --scan changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,14 +30,3 @@ pluginManagement {
         }
     }
 }
-
-plugins {
-    `gradle-enterprise`
-}
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
