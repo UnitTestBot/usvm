a75a75357fa15c9889accfc46b104d9b986f2fa0
Sergey Loktev <71882967+zishkaz@users.noreply.github.com>
2024-10-08 07:50:10 +0300

    TSTypeStorage commentary

--- usvm-ts/src/main/kotlin/org/usvm/TSTypeStorage.kt
+++ usvm-ts/src/main/kotlin/org/usvm/TSTypeStorage.kt
@@ -6,6 +6,10 @@ import org.jacodb.ets.base.EtsType
     This is a very basic implementation of type storage with memory and model objects interoperability.
     Currently, supports only stack register readings, but API-wise is finished.
 
+    Each untyped ref entity (reg reading, etc.) must define its own way to generate a key.
+    For reg readings it is suggested to use its 'idx' field since
+    they are sustained among all models and symbolic memory.
+
     TODO: support other possibly untyped refs.
  */
 class TSTypeStorage(
