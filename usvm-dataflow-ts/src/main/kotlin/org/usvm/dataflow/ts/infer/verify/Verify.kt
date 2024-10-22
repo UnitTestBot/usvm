a698cd23ccd2121337fb2f7f94cbf9829ab9e920
MForest7 <goalkeeper14631@gmail.com>
2024-10-22 17:34:38 +0300

    verify type consistency in scene

---
+++ usvm-dataflow-ts/src/main/kotlin/org/usvm/dataflow/ts/infer/verify/Verify.kt
@@ -0,0 +1,28 @@
+/*
+ *  Copyright 2022 UnitTestBot contributors (utbot.org)
+ * <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.usvm.dataflow.ts.infer.verify
+
+import org.jacodb.ets.base.EtsType
+import org.jacodb.ets.model.EtsScene
+import org.usvm.dataflow.ts.infer.verify.collectors.ClassSummaryCollector
+
+fun collectSummary(scene: EtsScene): Map<EntityId, Set<EtsType>> =
+    ClassSummaryCollector(mutableMapOf()).apply {
+        scene.classes.forEach { collect(it) }
+    }.typeSummary
+
+fun verify(scene: EtsScene) = VerificationResult.from(collectSummary(scene))
