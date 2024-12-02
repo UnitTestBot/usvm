c27b0cbb7a53e39efc8f66f480ce2e7a47d2e4bd
Emil <buraindo24@gmail.com>
2024-12-02 19:46:42 +0300

    Introduced dynamic tests

--- usvm-go/src/test/kotlin/org/usvm/JacoDbTest.kt
+++ usvm-go/src/test/kotlin/org/usvm/JacoDbTest.kt
@@ -1,6 +1,8 @@
 package org.usvm
 
-import org.junit.jupiter.api.Test
+import org.jacodb.go.api.GoMethod
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import org.usvm.model.Converter
 import org.usvm.model.Parser
 import kotlin.system.measureTimeMillis
@@ -8,160 +10,29 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
 class JacoDbTest {
-    @Test
-    fun testMax2() {
-        test("max2")
+    @TestFactory
+    fun jacodbTestLong(): Collection<DynamicTest> {
+        val pkg = Converter.unpackPackage(Parser().deserialize("out/usvm_examples.json"))
+        val machine = GoMachine(pkg, options)
+        return methods(pkg).filter { it.metName in arrayOf("loopInfinite", "loopInner", "loopCollatz") }.map {
+            DynamicTest.dynamicTest(it.metName) {
+                println(measureTimeMillis { machine.analyzeAndResolve(it.metName) })
+            }
+        }
     }
 
-    @Test
-    fun testMax2Anon() {
-        test("max2Anon")
+    @TestFactory
+    fun jacodbTestFast(): Collection<DynamicTest> {
+        val pkg = Converter.unpackPackage(Parser().deserialize("out/usvm_examples.json"))
+        val machine = GoMachine(pkg, options)
+        return methods(pkg).filter { it.metName !in arrayOf("loopInfinite", "loopInner", "loopCollatz") }.map {
+            DynamicTest.dynamicTest(it.metName) {
+                println(measureTimeMillis { machine.analyzeAndResolve(it.metName) })
+            }
+        }
     }
 
-    @Test
-    fun testMax2Closure() {
-        test("max2Closure")
-    }
-
-    @Test
-    fun testMax3() {
-        test("max3")
-    }
-
-    @Test
-    fun testMax3Call() {
-        test("max3Call")
-    }
-
-    @Test
-    fun testMax4Call() {
-        test("max4Call")
-    }
-
-    @Test
-    fun testInc() {
-        test("inc")
-    }
-
-    @Test
-    fun testBeforeAndAfter() {
-        test("beforeAndAfter")
-    }
-
-    @Test
-    fun testSumBeforeAndAfter() {
-        test("sumBeforeAndAfter")
-    }
-
-    @Test
-    fun testLoopSimple() {
-        test("loopSimple")
-    }
-
-    @Test
-    fun testLoopIf() {
-        test("loopIf")
-    }
-
-    @Test
-    fun testLoopInfinite() {
-        test("loopInfinite")
-    }
-
-    @Test
-    fun testLoopInner() {
-        test("loopInner")
-    }
-
-    @Test
-    fun testLoopCollatz() {
-        test("loopCollatz")
-    }
-
-    @Test
-    fun testLoopSum() {
-        test("loopSum")
-    }
-
-    @Test
-    fun testPointerSimple() {
-        test("pointerSimple")
-    }
-
-    @Test
-    fun testPointerOther() {
-        test("pointerOther")
-    }
-
-    @Test
-    fun testPointerAnother() {
-        test("pointerAnother")
-    }
-
-    @Test
-    fun testSliceSimple() {
-        test("sliceSimple")
-    }
-
-    @Test
-    fun testSliceOverwrite() {
-        test("sliceOverwrite")
-    }
-
-    @Test
-    fun testSliceAlloc() {
-        test("sliceAlloc")
-    }
-
-    @Test
-    fun testSliceFirst() {
-        test("sliceFirst")
-    }
-
-    @Test
-    fun testSliceSum() {
-        test("sliceSum")
-    }
-
-    @Test
-    fun testSliceCompare() {
-        test("sliceCompare")
-    }
-
-    @Test
-    fun testSliceCompareFuncVar() {
-        test("sliceCompareFuncVar")
-    }
-
-    @Test
-    fun testMapAlloc() {
-        test("mapAlloc")
-    }
-
-    @Test
-    fun testMapLookup() {
-        test("mapLookup")
-    }
-
-    @Test
-    fun testMapLookupComma() {
-        test("mapLookupComma")
-    }
-
-    @Test
-    fun testMapLookupCommaReturn() {
-        test("mapLookupCommaReturn")
-    }
-
-    @Test
-    fun testMapUpdate() {
-        test("mapUpdate")
-    }
-
-    @Test
-    fun testGlobalSimple() {
-        test("globalSimple")
-    }
+    private fun methods(pkg: GoPackage): List<GoMethod> = pkg.methods.filter { !it.metName.contains('$') }
 
     private val options: UMachineOptions = UMachineOptions(
         pathSelectionStrategies = listOf(PathSelectionStrategy.FORK_DEPTH),
@@ -172,14 +43,4 @@ class JacoDbTest {
         solverTimeout = Duration.INFINITE, // we do not need the timeout for a solver in tests
         typeOperationsTimeout = Duration.INFINITE, // we do not need the timeout for type operations in tests
     )
-
-    private fun test(name: String) {
-        val stopwatch = measureTimeMillis {
-            val pkg = Converter.unpackPackage(Parser().deserialize("out/usvm_examples.json"))
-            val machine = GoMachine(pkg, options)
-            println(machine.analyzeAndResolve(name))
-        }
-
-        println(stopwatch)
-    }
 }
