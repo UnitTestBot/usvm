80adfafed742b728d078eb57599d05948879528c
Konstantin Chukharev <lipen00@gmail.com>
2025-01-15 18:52:38 +0300

    Use jacodb/neo from jitpack

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val clikt = "5.0.0"
     const val detekt = "1.23.7"
     const val ini4j = "0.5.4"
-    const val jacodb = "ad5e1f170e"
+    const val jacodb = "983d3538a9"
     const val juliet = "1.3.2"
     const val junit = "5.9.3"
     const val kotlin = "2.1.0"
@@ -116,8 +116,7 @@ object Libs {
     )
 
     // https://github.com/UnitTestBot/jacodb
-    // private const val jacodbPackage = "com.github.UnitTestBot.jacodb" // use "org.jacodb" with includeBuild
-    private const val jacodbPackage = "org.jacodb"
+    private const val jacodbPackage = "com.github.UnitTestBot.jacodb" // use "org.jacodb" with includeBuild
     val jacodb_core = dep(
         group = jacodbPackage,
         name = "jacodb-core",
