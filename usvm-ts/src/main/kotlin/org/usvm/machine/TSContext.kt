8bbfedc6430f4c934d07d318307580696a8a2aaf
Alexey Menshutin <alex.menshutin99@gmail.com>
2025-01-16 17:26:25 +0300

    Tmp commit

--- usvm-ts/src/main/kotlin/org/usvm/machine/TSContext.kt
+++ usvm-ts/src/main/kotlin/org/usvm/machine/TSContext.kt
@@ -1,6 +1,5 @@
 package org.usvm.machine
 
-import io.ksmt.utils.cast
 import org.jacodb.ets.base.EtsBooleanType
 import org.jacodb.ets.base.EtsNumberType
 import org.jacodb.ets.base.EtsRefType
@@ -8,29 +7,14 @@ import org.jacodb.ets.base.EtsType
 import org.jacodb.ets.base.EtsUnknownType
 import org.usvm.UBv32Sort
 import org.usvm.UContext
-import org.usvm.UExpr
 import org.usvm.USort
 import org.usvm.machine.expr.TSUndefinedSort
 import org.usvm.machine.expr.TSUndefinedValue
 import org.usvm.machine.expr.TSUnresolvedSort
-import org.usvm.machine.expr.TSWrappedValue
 
 typealias TSSizeSort = UBv32Sort
 
 class TSContext(components: TSComponents) : UContext<TSSizeSort>(components) {
-
-    private val tsWrappedValueCache = mkAstInterner<TSWrappedValue>()
-    fun <T : USort> mkTSWrappedValue(value: UExpr<T>): TSWrappedValue = tsWrappedValueCache.createIfContextActive {
-        if (value is TSWrappedValue) return@createIfContextActive value
-
-        when (val valueSort = value.sort) {
-            boolSort -> TSWrappedValue(this, boolValue = value.cast())
-            fp64Sort -> TSWrappedValue(this, fpValue = value.cast())
-            addressSort -> TSWrappedValue(this, refValue = value.cast())
-            else -> error("Unsupported sort: $valueSort")
-        }
-    }.cast()
-
     val undefinedSort: TSUndefinedSort by lazy { TSUndefinedSort(this) }
 
     val unresolvedSort: TSUnresolvedSort = TSUnresolvedSort(this)
--- usvm-ts/src/main/kotlin/org/usvm/machine/expr/TSExpressions.kt
+++ usvm-ts/src/main/kotlin/org/usvm/machine/expr/TSExpressions.kt
@@ -4,19 +4,13 @@ import io.ksmt.KAst
 import io.ksmt.cache.hash
 import io.ksmt.cache.structurallyEqual
 import io.ksmt.expr.KBitVec32Value
-import io.ksmt.expr.KExpr
 import io.ksmt.expr.KFp64Value
 import io.ksmt.expr.printer.ExpressionPrinter
 import io.ksmt.expr.transformer.KTransformerBase
 import io.ksmt.sort.KSortVisitor
-import org.usvm.UAddressSort
-import org.usvm.UBoolSort
 import org.usvm.UExpr
-import org.usvm.UFpSort
 import org.usvm.USort
-import org.usvm.USymbol
 import org.usvm.machine.TSContext
-import org.usvm.uctx
 
 val KAst.tctx get() = ctx as TSContext
 
@@ -45,97 +39,10 @@ class TSUndefinedValue(ctx: TSContext) : UExpr<TSUndefinedSort>(ctx) {
 
 // TODO rename it
 class TSUnresolvedSort(ctx: TSContext) : USort(ctx) {
-    val possibleSorts: Set<USort> = mutableSetOf() // TODO ??????
-
     override fun <T> accept(visitor: KSortVisitor<T>): T = error("Should not be called")
 
     override fun print(builder: StringBuilder) {
         builder.append("Unresolved sort")
-    }
-}
-
-/**
- * [UExpr] wrapper that handles type coercion.
- *
- * @param value wrapped expression.
- */
-// TODO check that can occur only in assignStmt
-class TSWrappedValue(
-    ctx: TSContext,
-    val boolValue: UExpr<UBoolSort>? = null,
-    val fpValue: UExpr<UFpSort>? = null,
-    // TODO string, bigint
-    val refValue: UExpr<UAddressSort>? = null,
-) : USymbol<USort>(ctx) {
-    init {
-        require(boolValue != null || fpValue != null || refValue != null) {
-            "An empty TSWrappedValue is created"
-        }
-    }
-
-    override val sort: USort
-        get() = when {
-            boolValue != null && fpValue == null && refValue == null -> ctx.boolSort
-            boolValue == null && fpValue != null && refValue == null -> ctx.fp64Sort
-            boolValue == null && fpValue == null && refValue != null -> uctx.addressSort
-            else -> tctx.unresolvedSort
-        }
-
-    // fun asSort(
-    //     sort: USort,
-    //     scope: TSStepScope,
-    // ): UExpr<out USort>? = scope.calcOnState { exprTransformer.transform(value, sort) }
-    //
-    // private fun coerce(
-    //     other: UExpr<out USort>,
-    //     action: CoerceAction,
-    //     scope: TSStepScope,
-    // ): UExpr<out USort> = with(scope) {
-    //     when (other) {
-    //         is UIntepretedValue -> {
-    //             calcOnState {
-    //                 exprTransformer.intersectWithTypeCoercion(value, other, action, scope)
-    //             }
-    //         }
-    //
-    //         is TSWrappedValue -> {
-    //             calcOnState {
-    //                 exprTransformer.intersectWithTypeCoercion(value, other.value, action, scope)
-    //             }
-    //         }
-    //
-    //         else -> error("Unexpected $other in type coercion")
-    //     }
-    // }
-    //
-    // fun coerceWithSort(
-    //     other: UExpr<out USort>,
-    //     action: CoerceAction,
-    //     desiredSort: USort?,
-    //     scope: TSStepScope,
-    // ): UExpr<out USort> = with(scope) {
-    //     desiredSort?.let {
-    //         doWithState {
-    //             exprTransformer.transform(value, it)
-    //         }
-    //     }
-    //
-    //     return coerce(other, action, scope)
-    // }
-    //
-    override fun accept(transformer: KTransformerBase): KExpr<USort> {
-        error("Should not be called")
-    }
-
-    override fun internEquals(other: Any): Boolean = structurallyEqual(other, { boolValue }, { fpValue }, { refValue })
-
-    override fun internHashCode(): Int = hash(boolValue, fpValue, refValue)
-
-    override fun print(printer: ExpressionPrinter) {
-        printer.append("wrapped(")
-        // TODO modify
-        printer.append(listOfNotNull(boolValue, fpValue, refValue).joinToString(", "))
-        printer.append(")")
     }
 }
 
