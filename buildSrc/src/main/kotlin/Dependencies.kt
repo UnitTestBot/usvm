8b7f9b135220fff7ff0591837dd93b66672f4c11
Alexey Menshutin <alex.menshutin99@gmail.com>
2025-01-24 15:52:14 +0300

    Bump jacodb version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val clikt = "5.0.0"
     const val detekt = "1.23.7"
     const val ini4j = "0.5.4"
-    const val jacodb = "94421559de" // TODO bump it
+    const val jacodb = "8a588e71e7"
     const val juliet = "1.3.2"
     const val junit = "5.9.3"
     const val kotlin = "2.1.0"
