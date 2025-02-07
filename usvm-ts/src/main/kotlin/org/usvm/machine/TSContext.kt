36e73330d9371dbd78929a181767381e70914ae2
Konstantin Chukharev <lipen00@gmail.com>
2025-02-07 17:19:57 +0300

    Map undefined type to address sort

--- usvm-ts/src/main/kotlin/org/usvm/machine/TSContext.kt
+++ usvm-ts/src/main/kotlin/org/usvm/machine/TSContext.kt
@@ -5,6 +5,7 @@ import org.jacodb.ets.base.EtsBooleanType
 import org.jacodb.ets.base.EtsNumberType
 import org.jacodb.ets.base.EtsRefType
 import org.jacodb.ets.base.EtsType
+import org.jacodb.ets.base.EtsUndefinedType
 import org.jacodb.ets.base.EtsUnknownType
 import org.jacodb.ets.model.EtsScene
 import org.usvm.UAddressSort
@@ -40,6 +41,7 @@ class TSContext(
         is EtsBooleanType -> boolSort
         is EtsNumberType -> fp64Sort
         is EtsRefType -> addressSort
+        is EtsUndefinedType -> addressSort
         is EtsUnknownType -> unresolvedSort
         else -> TODO("Support all JacoDB types, encountered $type")
     }
