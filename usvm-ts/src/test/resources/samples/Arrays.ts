007bddf158430d5e24fcc89f5524365533972829
Konstantin Chukharev <lipen00@gmail.com>
2025-02-14 16:18:19 +0300

    Cleanup

--- usvm-ts/src/test/resources/samples/Arrays.ts
+++ usvm-ts/src/test/resources/samples/Arrays.ts
@@ -1,11 +1,7 @@
 class Arrays {
     createConstantArrayOfNumbers() {
         let x = [1, 2, 3]
-
-        if (x[0] == 1) {
-            return 1
-        }
-
+        if (x[0] == 1) return 1
         return -1
     }
 
