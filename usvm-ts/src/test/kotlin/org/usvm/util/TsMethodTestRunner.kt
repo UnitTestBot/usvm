caff766f2da698227bc82fecd2f5ffc6ae9b9ebe
Konstantin Chukharev <lipen00@gmail.com>
2025-02-17 18:48:47 +0300

    Format

--- usvm-ts/src/test/kotlin/org/usvm/util/TsMethodTestRunner.kt
+++ usvm-ts/src/test/kotlin/org/usvm/util/TsMethodTestRunner.kt
@@ -167,15 +167,18 @@ abstract class TsMethodTestRunner : TestRunner<TsTest, EtsMethod, EtsType?, TsMe
         when (klass) {
             TsValue::class -> EtsAnyType
             TsValue.TsAny::class -> EtsAnyType
+
             TsValue.TsArray::class -> {
                 EtsArrayType(EtsAnyType, dimensions = 1) // TODO incorrect
-            }            TsValue.TsBoolean::class -> EtsBooleanType
+            }
+
             TsValue.TsClass::class -> {
                 // TODO incorrect
                 val signature = EtsClassSignature(it.toString(), EtsFileSignature.DEFAULT)
                 EtsClassType(signature)
             }
 
+            TsValue.TsBoolean::class -> EtsBooleanType
             TsValue.TsString::class -> EtsStringType
             TsValue.TsNumber::class -> EtsNumberType
             TsValue.TsNumber.TsDouble::class -> EtsNumberType
@@ -186,6 +189,7 @@ abstract class TsMethodTestRunner : TestRunner<TsTest, EtsMethod, EtsType?, TsMe
             // For untyped tests, not to limit objects serialized from models after type coercion.
             TsValue.TsUnknown::class -> EtsUnknownType
             TsValue.TsNull::class -> EtsNullType
+
             TsValue.TsException::class -> {
                 // TODO incorrect
                 val signature = EtsClassSignature("Exception", EtsFileSignature.DEFAULT)
