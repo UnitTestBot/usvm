d023b91a372f3ee73b0d17eab17c5345d5b7cb6e
Konstantin Chukharev <lipen00@gmail.com>
2025-02-24 17:19:02 +0300

    Extract common code for loading sample scene

--- usvm-ts/src/test/kotlin/org/usvm/samples/MinValue.kt
+++ usvm-ts/src/test/kotlin/org/usvm/samples/MinValue.kt
@@ -1,23 +1,16 @@
 package org.usvm.samples
 
 import org.jacodb.ets.model.EtsScene
-import org.jacodb.ets.utils.loadEtsFileAutoConvert
 import org.junit.jupiter.api.Disabled
 import org.usvm.api.TsValue
 import org.usvm.util.TsMethodTestRunner
-import org.usvm.util.getResourcePath
 import kotlin.test.Test
 
 class MinValue : TsMethodTestRunner() {
 
     private val className = this::class.simpleName!!
 
-    override val scene: EtsScene = run {
-        val name = "$className.ts"
-        val path = getResourcePath("/samples/$name")
-        val file = loadEtsFileAutoConvert(path)
-        EtsScene(listOf(file))
-    }
+    override val scene: EtsScene = loadSampleScene(className)
 
     @Test
     @Disabled
