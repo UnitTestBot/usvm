252fb0cf092b88579090e80c2bcb5373c92876e6
Konstantin Chukharev <lipen00@gmail.com>
2025-02-24 18:41:25 +0300

    Add call/field resolver, disable AA type inference

--- usvm-ts/src/main/kotlin/org/usvm/util/Utils.kt
+++ usvm-ts/src/main/kotlin/org/usvm/util/Utils.kt
@@ -2,8 +2,6 @@ package org.usvm.util
 
 import io.ksmt.sort.KFp64Sort
 import org.jacodb.ets.base.EtsType
-import org.jacodb.ets.model.EtsFieldSignature
-import org.jacodb.ets.model.EtsScene
 import org.usvm.UBoolSort
 import org.usvm.UExpr
 import org.usvm.UHeapRef
@@ -14,12 +12,6 @@ import org.usvm.machine.state.TsState
 // Built-in KContext.bvToBool has identical implementation.
 fun TsContext.boolToFp(expr: UExpr<UBoolSort>): UExpr<KFp64Sort> =
     mkIte(expr, mkFp64(1.0), mkFp64(0.0))
-
-// TODO probably this should be written differently
-fun EtsScene.fieldLookUp(field: EtsFieldSignature) = projectAndSdkClasses
-    .first { it.signature == field.enclosingClass }
-    .fields
-    .single { it.name == field.name }
 
 fun TsState.throwExceptionWithoutStackFrameDrop(address: UHeapRef, type: EtsType) {
     methodResult = TsMethodResult.TsException(address, type)
