885973cd63f9fa40adb40395f9c59c59f4be7733
Konstantin Chukharev <lipen00@gmail.com>
2025-02-27 15:35:28 +0300

    Cleanup

--- usvm-ts/src/main/kotlin/org/usvm/util/Utils.kt
+++ usvm-ts/src/main/kotlin/org/usvm/util/Utils.kt
@@ -2,19 +2,12 @@ package org.usvm.util
 
 import io.ksmt.sort.KFp64Sort
 import org.jacodb.ets.base.EtsType
-import org.usvm.UBoolExpr
 import org.usvm.UBoolSort
-import org.usvm.UConcreteHeapRef
 import org.usvm.UExpr
 import org.usvm.UHeapRef
-import org.usvm.api.typeStreamOf
 import org.usvm.machine.TsContext
-import org.usvm.machine.interpreter.TsStepScope
 import org.usvm.machine.state.TsMethodResult
 import org.usvm.machine.state.TsState
-import org.usvm.machine.types.FakeType
-import org.usvm.types.UTypeStream
-import org.usvm.types.single
 
 // Built-in KContext.bvToBool has identical implementation.
 fun TsContext.boolToFp(expr: UExpr<UBoolSort>): UExpr<KFp64Sort> =
