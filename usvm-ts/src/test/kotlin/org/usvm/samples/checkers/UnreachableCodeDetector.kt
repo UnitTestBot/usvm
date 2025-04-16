6c80308046f89eefaefcff3420a8827f961aed9a
Alexey Menshutin <alex.menshutin99@gmail.com>
2025-04-16 13:12:37 +0300

    Tests update

--- usvm-ts/src/test/kotlin/org/usvm/samples/checkers/UnreachableCodeDetector.kt
+++ usvm-ts/src/test/kotlin/org/usvm/samples/checkers/UnreachableCodeDetector.kt
@@ -11,18 +11,18 @@ import org.usvm.machine.TsOptions
 import org.usvm.util.getResourcePath
 
 class UnreachableCodeDetectorTest {
+    val scene = run {
+        val name = "UnreachableCode.ts"
+        val path = getResourcePath("/samples/checkers/$name")
+        val file = loadEtsFileAutoConvert(
+            path,
+            useArkAnalyzerTypeInference = 1
+        )
+        EtsScene(listOf(file))
+    }
+
     @Test
     fun testUnreachableCode() {
-        val scene = run {
-            val name = "UnreachableCode.ts"
-            val path = getResourcePath("/samples/checkers/$name")
-            val file = loadEtsFileAutoConvert(
-                path,
-                useArkAnalyzerTypeInference = 1
-            )
-            EtsScene(listOf(file))
-        }
-
         val options = UMachineOptions()
         val tsOptions = TsOptions(interproceduralAnalysis = false)
         val observer = UnreachableCodeDetector()
@@ -30,9 +30,12 @@ class UnreachableCodeDetectorTest {
         val methods = scene.projectClasses
             .flatMap { it.methods }
             .filterNot { it.cfg.stmts.isEmpty() }
-            //.filter { it.name == "simpleUnreachableBranch" }
-        val results = machine.analyze(methods)
+            .filter { it.name == "simpleUnreachableBranch" }
+        machine.analyze(methods)
 
-        check(results.isNotEmpty())
+        val uncoveredResults = observer.result.values.singleOrNull() ?: error("No results found")
+        val uncoveredStatements = uncoveredResults.singleOrNull()
+
+        check(uncoveredStatements != null) { "Uncovered statements are incorrect, results are $uncoveredStatements" }
     }
 }
