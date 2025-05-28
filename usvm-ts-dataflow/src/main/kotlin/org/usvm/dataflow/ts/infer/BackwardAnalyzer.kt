094d735e91c0a42bb79072f7f50d1d1e96d2df28
MForest7 <goalkeeper14631@gmail.com>
2025-05-28 18:43:54 +0300

    removed old backward flow functions

--- usvm-ts-dataflow/src/main/kotlin/org/usvm/dataflow/ts/infer/BackwardAnalyzer.kt
+++ usvm-ts-dataflow/src/main/kotlin/org/usvm/dataflow/ts/infer/BackwardAnalyzer.kt
@@ -2,8 +2,6 @@ package org.usvm.dataflow.ts.infer
 
 import org.jacodb.ets.model.EtsMethod
 import org.jacodb.ets.model.EtsStmt
-import org.jacodb.ets.model.EtsType
-import org.jacodb.impl.cfg.graphs.GraphDominators
 import org.usvm.dataflow.ifds.Analyzer
 import org.usvm.dataflow.ifds.Edge
 import org.usvm.dataflow.ifds.Vertex
@@ -11,23 +9,21 @@ import org.usvm.dataflow.ts.graph.EtsApplicationGraph
 
 class BackwardAnalyzer(
     val graph: EtsApplicationGraph,
-    savedTypes: MutableMap<EtsType, MutableList<EtsTypeFact>>,
-    dominators: (EtsMethod) -> GraphDominators<EtsStmt>,
     doAddKnownTypes: Boolean = true,
-) : Analyzer<BackwardPathTypeDomainFact, AnalyzerEvent, EtsMethod, EtsStmt> {
+) : Analyzer<BackwardTypeDomainFact, AnalyzerEvent, EtsMethod, EtsStmt> {
 
-    override val flowFunctions = BackwardPathFlowFunctions(doAddKnownTypes)
+    override val flowFunctions = BackwardFlowFunctions(doAddKnownTypes)
 
     override fun handleCrossUnitCall(
-        caller: Vertex<BackwardPathTypeDomainFact, EtsStmt>,
-        callee: Vertex<BackwardPathTypeDomainFact, EtsStmt>,
+        caller: Vertex<BackwardTypeDomainFact, EtsStmt>,
+        callee: Vertex<BackwardTypeDomainFact, EtsStmt>,
     ): List<AnalyzerEvent> {
         error("No cross unit calls")
     }
 
-    override fun handleNewEdge(edge: Edge<BackwardPathTypeDomainFact, EtsStmt>): List<AnalyzerEvent> {
+    override fun handleNewEdge(edge: Edge<BackwardTypeDomainFact, EtsStmt>): List<AnalyzerEvent> {
         val (startVertex, currentVertex) = edge
-        val (current, currentFact) = currentVertex
+        val (current, _) = currentVertex
 
         val method = graph.methodOf(current)
         val currentIsExit = current in graph.exitPoints(method)
