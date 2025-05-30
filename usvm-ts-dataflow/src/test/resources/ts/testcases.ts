b02da2971ecd118ec867ee52464d3cb339b3f5ca
Artem Trubnikov <77829679+MForest7@users.noreply.github.com>
2025-05-30 16:10:42 +0300

    Add special objects handlers to type processor (#279)
    ---------
    
    Co-authored-by: Alexey Menshutin <alex.menshutin99@gmail.com>

--- usvm-ts-dataflow/src/test/resources/ts/testcases.ts
+++ usvm-ts-dataflow/src/test/resources/ts/testcases.ts
@@ -215,6 +215,36 @@ class CaseAssignLocalToArrayElementNumber {
 
 // ----------------------------------------
 
+// Case `y := x.length, x: any[]`
+class CaseAssignLocalToArrayLength {
+    entrypoint(x: any[]) {
+        let y = x.length; // y: number
+        x[0] = y; // x: Array<any>
+        this.infer(x);
+    }
+
+    infer(a: any) {
+        const EXPECTED_ARG_0 = "Array<any>";
+    }
+}
+
+// ----------------------------------------
+
+// Case `y := x.length, x: string`
+class CaseAssignLocalToStringLength {
+    entrypoint(x: any) {
+        x = "abacaba"; // x: string
+        let y = x.length; // y: number
+        this.infer(x);
+    }
+
+    infer(a: any) {
+        const EXPECTED_ARG_0 = "string";
+    }
+}
+
+// ----------------------------------------
+
 interface ICustom {
     a: number;
     b: string;
