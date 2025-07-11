b0dcc31d5eacec703fbca62884ec26cc4a8447d5
Alexey Menshutin <alex.menshutin99@gmail.com>
2025-07-11 13:48:05 +0300

    Reachability prototype

--- usvm-ts/src/main/kotlin/org/usvm/api/targets/TsTarget.kt
+++ usvm-ts/src/main/kotlin/org/usvm/api/targets/TsTarget.kt
@@ -3,4 +3,10 @@ package org.usvm.api.targets
 import org.jacodb.ets.model.EtsStmt
 import org.usvm.targets.UTarget
 
-class TsTarget : UTarget<EtsStmt, TsTarget>()
+open class TsTarget(location: EtsStmt?) : UTarget<EtsStmt, TsTarget>(location)
+
+sealed class TsReachabilityTarget(override val location: EtsStmt) : TsTarget(location) {
+    class InitialPoint(location: EtsStmt) : TsReachabilityTarget(location)
+    class IntermediatePoint(location: EtsStmt) : TsReachabilityTarget(location)
+    class FinalPoint(location: EtsStmt) : TsReachabilityTarget(location)
+}
