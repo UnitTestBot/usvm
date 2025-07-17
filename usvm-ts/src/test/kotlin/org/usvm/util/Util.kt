18dc0ae4295e4e59a3153591cda34cb2ce90560f
Konstantin Chukharev <lipen00@gmail.com>
2025-07-17 19:11:20 +0300

    Add tests (disabled) for closures capturing mutable locals

--- usvm-ts/src/test/kotlin/org/usvm/util/Util.kt
+++ usvm-ts/src/test/kotlin/org/usvm/util/Util.kt
@@ -1,3 +1,13 @@
 package org.usvm.util
 
+import kotlin.math.abs
+
 fun Boolean.toDouble() = if (this) 1.0 else 0.0
+
+infix fun Double.eq(other: Int): Boolean {
+    return this eq other.toDouble()
+}
+
+infix fun Double.eq(other: Double): Boolean {
+    return abs(this - other) < 1e-9
+}
