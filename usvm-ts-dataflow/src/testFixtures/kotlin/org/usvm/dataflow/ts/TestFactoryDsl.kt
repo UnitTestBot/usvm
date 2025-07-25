95200a7fa79048285ebb1870d85dcb95c2e37ce5
Konstantin Chukharev <lipen00@gmail.com>
2025-07-25 18:08:12 +0300

    Allow nested dynamic tests
    Avoid "concurrent modification errors" in list iterator by wrapping the list with a lazy generating sequence

--- usvm-ts-dataflow/src/testFixtures/kotlin/org/usvm/dataflow/ts/TestFactoryDsl.kt
+++ usvm-ts-dataflow/src/testFixtures/kotlin/org/usvm/dataflow/ts/TestFactoryDsl.kt
@@ -3,7 +3,6 @@ package org.usvm.dataflow.ts
 import org.junit.jupiter.api.DynamicContainer
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
-import java.util.stream.Stream
 
 class TestContainerBuilder(var name: String) {
     private val nodes: MutableList<DynamicNode> = mutableListOf()
@@ -17,7 +16,7 @@ class TestContainerBuilder(var name: String) {
     }
 
     fun build(): DynamicContainer {
-        return DynamicContainer.dynamicContainer(name, nodes)
+        return DynamicContainer.dynamicContainer(name, nodes.trick())
     }
 }
 
@@ -26,6 +25,11 @@ private fun dynamicTest(name: String, test: () -> Unit): DynamicTest =
 
 private fun dynamicContainer(name: String, init: TestContainerBuilder.() -> Unit): DynamicContainer =
     TestContainerBuilder(name).apply(init).build()
+
+private fun <T> List<T>.trick(): Iterable<T> {
+    var i = 0
+    return generateSequence { getOrNull(i++) }.asIterable()
+}
 
 class TestFactoryBuilder {
     private val nodes: MutableList<DynamicNode> = mutableListOf()
@@ -38,10 +42,10 @@ class TestFactoryBuilder {
         nodes += dynamicContainer(name, init)
     }
 
-    fun build(): Stream<out DynamicNode> {
-        return nodes.stream()
+    fun build(): Iterable<DynamicNode> {
+        return nodes.trick()
     }
 }
 
-fun testFactory(init: TestFactoryBuilder.() -> Unit): Stream<out DynamicNode> =
+fun testFactory(init: TestFactoryBuilder.() -> Unit): Iterable<DynamicNode> =
     TestFactoryBuilder().apply(init).build()
