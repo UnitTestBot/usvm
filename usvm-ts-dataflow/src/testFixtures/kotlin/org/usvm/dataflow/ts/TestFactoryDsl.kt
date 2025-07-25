84571bb1ae6ca02a04ce0f8da70d69745e1b542c
Konstantin Chukharev <lipen00@gmail.com>
2025-07-25 15:53:03 +0300

    Refactor dynamic test factory

--- usvm-ts-dataflow/src/testFixtures/kotlin/org/usvm/dataflow/ts/TestFactoryDsl.kt
+++ usvm-ts-dataflow/src/testFixtures/kotlin/org/usvm/dataflow/ts/TestFactoryDsl.kt
@@ -3,43 +3,37 @@ package org.usvm.dataflow.ts
 import org.junit.jupiter.api.DynamicContainer
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.function.Executable
 import java.util.stream.Stream
 
-private interface TestProvider {
-    fun test(name: String, test: () -> Unit)
-}
+private fun dynamicTest(name: String, test: () -> Unit): DynamicTest =
+    DynamicTest.dynamicTest(name, test)
 
-private interface ContainerProvider {
-    fun container(name: String, init: TestContainerBuilder.() -> Unit)
-}
-
-class TestContainerBuilder(var name: String) : TestProvider, ContainerProvider {
+class TestContainerBuilder(var name: String) {
     private val nodes: MutableList<DynamicNode> = mutableListOf()
 
-    override fun test(name: String, test: () -> Unit) {
+    fun test(name: String, test: () -> Unit) {
         nodes += dynamicTest(name, test)
     }
 
-    override fun container(name: String, init: TestContainerBuilder.() -> Unit) {
-        nodes += containerBuilder(name, init)
+    fun container(name: String, init: TestContainerBuilder.() -> Unit) {
+        nodes += dynamicContainer(name, init)
     }
 
     fun build(): DynamicContainer = DynamicContainer.dynamicContainer(name, nodes)
 }
 
-private fun containerBuilder(name: String, init: TestContainerBuilder.() -> Unit): DynamicContainer =
+private fun dynamicContainer(name: String, init: TestContainerBuilder.() -> Unit): DynamicContainer =
     TestContainerBuilder(name).apply(init).build()
 
-class TestFactoryBuilder : TestProvider, ContainerProvider {
+class TestFactoryBuilder {
     private val nodes: MutableList<DynamicNode> = mutableListOf()
 
-    override fun test(name: String, test: () -> Unit) {
+    fun test(name: String, test: () -> Unit) {
         nodes += dynamicTest(name, test)
     }
 
-    override fun container(name: String, init: TestContainerBuilder.() -> Unit) {
-        nodes += containerBuilder(name, init)
+    fun container(name: String, init: TestContainerBuilder.() -> Unit) {
+        nodes += dynamicContainer(name, init)
     }
 
     fun build(): Stream<out DynamicNode> = nodes.stream()
@@ -47,6 +41,3 @@ class TestFactoryBuilder : TestProvider, ContainerProvider {
 
 fun testFactory(init: TestFactoryBuilder.() -> Unit): Stream<out DynamicNode> =
     TestFactoryBuilder().apply(init).build()
-
-private fun dynamicTest(name: String, test: () -> Unit): DynamicTest =
-    DynamicTest.dynamicTest(name, Executable(test))
--- usvm-ts/src/test/kotlin/org/usvm/project/RunOnAllProjects.kt
+++ usvm-ts/src/test/kotlin/org/usvm/project/RunOnAllProjects.kt
@@ -1,6 +1,7 @@
 package org.usvm.project
 
 import mu.KotlinLogging
+import org.jacodb.ets.model.EtsFile
 import org.jacodb.ets.model.EtsScene
 import org.jacodb.ets.utils.ANONYMOUS_CLASS_PREFIX
 import org.jacodb.ets.utils.ANONYMOUS_METHOD_PREFIX
@@ -40,8 +41,7 @@ class RunOnAllProjects {
 
         val machineOptions: UMachineOptions = UMachineOptions(
             pathSelectionStrategies = listOf(PathSelectionStrategy.CLOSEST_TO_UNCOVERED_RANDOM),
-            exceptionsPropagation = true,
-            timeout = 1000000000.seconds,
+            timeout = 10.seconds,
             stepsFromLastCovered = 3500L,
             solverType = SolverType.YICES,
             solverTimeout = Duration.INFINITE, // we do not need the timeout for a solver in tests
@@ -49,14 +49,22 @@ class RunOnAllProjects {
         )
     }
 
-    private fun createScene(projectName: String): EtsScene {
-        val projectPath = "$PROJECTS_ROOT/$projectName/source/entry"
-        val project = loadEtsProjectAutoConvert(getResourcePath(projectPath))
-        val sdkFiles = listOf(SDK_TS_PATH, SDK_OHOS_PATH).flatMap { sdk ->
+    private val sdkFiles: List<EtsFile> =
+        listOf(SDK_TS_PATH, SDK_OHOS_PATH).flatMap { sdk ->
+            logger.info { "Loading SDK from path: $sdk" }
             val sdkPath = getResourcePath(sdk)
             val sdkProject = loadEtsProjectAutoConvert(sdkPath, useArkAnalyzerTypeInference = null)
             sdkProject.projectFiles
+        }.also {
+            logger.info { "Loaded total ${it.size} SDK files" }
         }
+
+    private fun createScene(projectName: String): EtsScene {
+        logger.info { "Creating scene for project: $projectName" }
+        val projectPath = "$PROJECTS_ROOT/$projectName/source"
+        logger.info { "Loading project from path: $projectPath" }
+        val project = loadEtsProjectAutoConvert(getResourcePath(projectPath))
+        logger.info { "Loaded project $projectName with ${project.projectFiles.size} files" }
         return EtsScene(project.projectFiles, sdkFiles, projectName = project.projectName)
     }
 
@@ -69,64 +77,68 @@ class RunOnAllProjects {
         logger.info { "Found ${projects.size} projects: ${projects.joinToString(", ")}" }
 
         for (projectName in projects) {
-            logger.info { "Processing project: $projectName" }
-
-            test("Run on each class for $projectName") {
-                logger.info { "Running tests on each class in project: $projectName" }
+            container(projectName) {
+                logger.info { "Processing project: $projectName" }
                 val scene = createScene(projectName)
-                val exceptions = mutableListOf<Throwable>()
-                val classes = scene.projectClasses
-                    .filterNot { it.name.startsWith(ANONYMOUS_CLASS_PREFIX) }
-                logger.info { "Running on ${classes.size} classes for project $projectName" }
 
-                for (cls in classes) {
-                    val methods = cls.methods
-                        .filterNot { it.cfg.stmts.isEmpty() }
-                        .filterNot { it.isStatic }
-                        .filterNot { it.name.startsWith(ANONYMOUS_METHOD_PREFIX) }
-                        .filterNot { it.name == "build" }
-                        .filterNot { it.name == INSTANCE_INIT_METHOD_NAME }
-                        .filterNot { it.name == STATIC_INIT_METHOD_NAME }
-                        .filterNot { it.name == CONSTRUCTOR_NAME }
-                    if (methods.isEmpty()) continue
-                    logger.info { "Running on class ${cls.name} with ${methods.size} methods" }
+                container("Run on each class for $projectName") {
+                    logger.info { "Running tests on each class in project: $projectName" }
+                    val exceptions = mutableListOf<Throwable>()
+                    val classes = scene.projectClasses
+                        .filterNot { it.name.startsWith(ANONYMOUS_CLASS_PREFIX) }
+                    logger.info { "Running on ${classes.size} classes for project $projectName" }
 
-                    runCatching {
-                        val tsOptions = TsOptions()
-                        TsMachine(scene, machineOptions, tsOptions).use { machine ->
-                            val states = machine.analyze(methods)
-                            states.let {}
+                    for (cls in classes) {
+                        test("Run on class ${cls.name}") {
+                            logger.info { "Running on all methods in class $cls" }
+                            val methods = cls.methods
+                                .filterNot { it.cfg.stmts.isEmpty() }
+                                .filterNot { it.isStatic }
+                                .filterNot { it.name.startsWith(ANONYMOUS_METHOD_PREFIX) }
+                                .filterNot { it.name == "build" }
+                                .filterNot { it.name == INSTANCE_INIT_METHOD_NAME }
+                                .filterNot { it.name == STATIC_INIT_METHOD_NAME }
+                                .filterNot { it.name == CONSTRUCTOR_NAME }
+                            if (methods.isEmpty()) return@test
+                            logger.info { "Running on ${methods.size} methods in class $cls" }
+
+                            runCatching {
+                                val tsOptions = TsOptions()
+                                TsMachine(scene, machineOptions, tsOptions).use { machine ->
+                                    val states = machine.analyze(methods)
+                                    states.let {}
+                                }
+                            }.onFailure {
+                                exceptions += it
+                            }
                         }
-                    }.onFailure {
-                        exceptions += it
+                    }
+
+                    val exc = exceptions.groupBy { it }
+                    logger.info { "Total exceptions: ${exc.size}" }
+                    for (es in exc.values.sortedBy { it.size }.asReversed()) {
+                        logger.info { "${es.first()}" }
                     }
                 }
 
-                val exc = exceptions.groupBy { it }
-                logger.info { "Total exceptions: ${exc.size}" }
-                for (es in exc.values.sortedBy { it.size }.asReversed()) {
-                    logger.info { "${es.first()}" }
-                }
-            }
+                test("Run on all methods for $projectName") {
+                    logger.info { "Running on all methods in project: $projectName" }
+                    val methods = scene.projectClasses
+                        .filterNot { it.name.startsWith(ANONYMOUS_CLASS_PREFIX) }
+                        .flatMap { cls ->
+                            cls.methods
+                                .filterNot { it.cfg.stmts.isEmpty() }
+                                .filterNot { it.isStatic }
+                                .filterNot { it.name.startsWith(ANONYMOUS_METHOD_PREFIX) }
+                                .filterNot { it.name == "build" }
+                        }
+                    logger.info { "Running on ${methods.size} methods for project $projectName" }
 
-            test("Run on all methods for $projectName") {
-                logger.info { "Running tests on all methods in project: $projectName" }
-                val scene = createScene(projectName)
-                val methods = scene.projectClasses
-                    .filterNot { it.name.startsWith(ANONYMOUS_CLASS_PREFIX) }
-                    .flatMap { cls ->
-                        cls.methods
-                            .filterNot { it.cfg.stmts.isEmpty() }
-                            .filterNot { it.isStatic }
-                            .filterNot { it.name.startsWith(ANONYMOUS_METHOD_PREFIX) }
-                            .filterNot { it.name == "build" }
+                    val tsOptions = TsOptions()
+                    TsMachine(scene, machineOptions, tsOptions).use { machine ->
+                        val states = machine.analyze(methods)
+                        states.let {}
                     }
-                logger.info { "Running on ${methods.size} methods for project $projectName" }
-
-                val tsOptions = TsOptions()
-                TsMachine(scene, machineOptions, tsOptions).use { machine ->
-                    val states = machine.analyze(methods)
-                    states.let {}
                 }
             }
         }
