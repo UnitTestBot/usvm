8a577a833e87f493cab582362a1eeeefd7c396d3
Konstantin Chukharev <lipen00@gmail.com>
2025-07-29 14:26:47 +0300

    Fix forced forking via if-stmts. Fix tests by not using 'eq'

--- usvm-ts/src/test/kotlin/org/usvm/samples/lang/Arg.kt
+++ usvm-ts/src/test/kotlin/org/usvm/samples/lang/Arg.kt
@@ -1,12 +1,9 @@
 package org.usvm.samples.lang
 
 import org.jacodb.ets.model.EtsScene
-import org.junit.jupiter.api.Disabled
 import org.usvm.api.TsTestValue
 import org.usvm.util.TsMethodTestRunner
-import org.usvm.util.eq
 import org.usvm.util.isNaN
-import org.usvm.util.neq
 import kotlin.test.Test
 
 class Arg : TsMethodTestRunner() {
@@ -14,19 +11,20 @@ class Arg : TsMethodTestRunner() {
 
     override val scene: EtsScene = loadScene(tsPath)
 
-    @Disabled("Forking void intercept is not implemented yet")
     @Test
     fun `test number arg`() {
         val method = getMethod("numberArg")
         discoverProperties<TsTestValue.TsNumber, TsTestValue.TsNumber>(
             method = method,
             { a, r -> a.isNaN() },
-            { a, r -> a eq 0 },
-            { a, r -> a eq 1 },
-            { a, r -> !a.isNaN() && (a neq 0) && (a neq 1) },
+            { a, r -> a.number == 0.0 },
+            { a, r -> a.number == 1.0 },
+            { a, r ->
+                !a.isNaN() && (a.number != 0.0) && (a.number != 1.0)
+            },
             invariants = arrayOf(
                 { a, r ->
-                    if (a.isNaN()) r.isNaN() else (a eq r)
+                    if (a.isNaN()) r.isNaN() else (a.number == r.number)
                 }
             )
         )
--- usvm-ts/src/test/resources/samples/lang/Arg.ts
+++ usvm-ts/src/test/resources/samples/lang/Arg.ts
@@ -3,9 +3,9 @@
 
 class Arg {
     numberArg(a: number): number {
-        void Number.isNaN(a);
-        void a == 0;
-        void a == 1;
-        return a; // 4 cases: NaN, 0, 1, other number
+        if (Number.isNaN(a) == true) return a;
+        if (a == 0) return a;
+        if (a == 1) return a;
+        return a;
     }
 }
