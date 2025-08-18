464fb23839a63224c96d11145d75588e458272fc
Konstantin Chukharev <lipen00@gmail.com>
2025-08-18 20:20:54 +0300

    Bump jacodb

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val clikt = "5.0.0"
     const val detekt = "1.23.7"
     const val ini4j = "0.5.4"
-    const val jacodb = "0a50288d6d"
+    const val jacodb = "adb9706f4f"
     const val juliet = "1.3.2"
     const val junit = "5.9.3"
     const val kotlin = "2.1.0"
