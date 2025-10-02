3f878f4dcce1325d2289c9ae9ea58b33e7653b20
Konstantin Chukharev <lipen00@gmail.com>
2025-10-02 15:19:30 +0300

    Use unresolved sort for enum field reading

--- usvm-ts/src/main/kotlin/org/usvm/machine/expr/ReadField.kt
+++ usvm-ts/src/main/kotlin/org/usvm/machine/expr/ReadField.kt
@@ -2,6 +2,7 @@ package org.usvm.machine.expr
 
 import io.ksmt.utils.asExpr
 import mu.KotlinLogging
+import org.jacodb.ets.model.EtsClassCategory
 import org.jacodb.ets.model.EtsFieldSignature
 import org.jacodb.ets.model.EtsInstanceFieldRef
 import org.jacodb.ets.model.EtsLocal
@@ -78,7 +79,14 @@ fun TsContext.readField(
             addressSort
         }
 
-        is TsResolutionResult.Unique -> typeToSort(etsField.property.type)
+        is TsResolutionResult.Unique -> {
+            val cls = etsField.property.declaringClass
+            if (cls != null && cls.category == EtsClassCategory.ENUM) {
+                unresolvedSort
+            } else {
+                typeToSort(etsField.property.type)
+            }
+        }
 
         is TsResolutionResult.Ambiguous -> unresolvedSort
     }
