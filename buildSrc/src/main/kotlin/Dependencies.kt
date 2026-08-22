c0f9a5bac9b492ab8d6950ab1caae67dfb37e510
Aleksei Menshutin <a.menshutin@vkteam.ru>
2026-08-22 17:27:15 +0300

    [TS] Pin published native frontend artifact

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val clikt = "5.0.0"
     const val detekt = "1.23.7"
     const val ini4j = "0.5.4"
-    const val jacodb = "c4f57f058c"
+    const val jacodb = "f0415b7cdc"
     const val juliet = "1.3.2"
     const val junit = "5.9.3"
     const val kotlin = "2.1.0"
